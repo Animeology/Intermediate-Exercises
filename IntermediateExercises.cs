5de6d92d087400450c59668fd0efaa64efefcb6d
Animeology <joseph.nguyen19@yahoo.com>
2023-06-02 17:23:34 -0500

    Right align triangle lesson

--- IntermediateExercises.cs
+++ IntermediateExercises.cs
@@ -72,13 +72,14 @@
         Console.WriteLine("Input a string and it will make a right-aligned triangle");
 
         string input = Console.ReadLine()!;        
-        int height = input.Length;
+        int height = input.Length - 1;
+        int width = 1;
 
         Console.WriteLine();
-        for(int i = 0; i < height; i++)
+        for(int i = height; i >= 0; i--)
         {
-            Console.WriteLine(input[i]);
-            //Console.WriteLine();
+            Console.WriteLine(input.Substring(i, width));
+            width++;
         }
     }
 }
