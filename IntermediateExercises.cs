50e8af86f8d82554f0a0e5ac0be110b29a9aa36e
Animeology <joseph.nguyen19@yahoo.com>
2023-06-02 17:35:48 -0500

    Multiple of 3s and 5s and rearrange functions in order

--- IntermediateExercises.cs
+++ IntermediateExercises.cs
@@ -2,28 +2,90 @@
 {
     static void Main(string[] args)
     {
-        GraphicFunction();
+        MultipleThreesAndFives();
+        // GraphicFunction();
         // RightAlignTriangle();
         // NETriangle();
         // Triangle();
         // CalculateFunction();
     }
 
-    static void CalculateFunction()
+    static void MultipleThreesAndFives()
     {
-        Console.WriteLine("Input two numbers for a range to calculate the following: y = x^2 - 2x + 1");
+        Console.WriteLine("Prints out numbers ranged from 1 to 500 that are multiples of 3 and 5");
+
+        int x = 1;
+        int y = 500;
+
+        for (int i = x; i <= y; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.Write("{0} ", i);
+            }
+        }
+    }
+
+    static void GraphicFunction()
+    {
+        Console.WriteLine("Input two numbers for a range to calculate the following: y = (x - 4)^2");
 
         int x = Convert.ToInt32(Console.ReadLine());
         int y = Convert.ToInt32(Console.ReadLine());
 
         for (int i = x; i <= y; i++)
         {
-            double function = Math.Pow(i, 2) - (2 * i) + 1;
+            double function = (i - 4);
+            double graphicFunc = Math.Pow(function, 2);
 
-            Console.Write("{0} ", function);
+            for (int j = 0; j < graphicFunc; j++)
+            {
+                Console.Write('*');
+            }
+            Console.WriteLine();
         }
     }
 
+    static void RightAlignTriangle()
+    {
+        Console.WriteLine("Input a string and it will make a right-aligned triangle");
+
+        string input = Console.ReadLine()!;
+        int height = input.Length - 1;
+        int width = 1;
+
+        Console.WriteLine();
+        for (int i = height; i >= 0; i--)
+        {
+            Console.WriteLine(input.Substring(i, width));
+            width++;
+        }
+    }
+
+    static void NETriangle()
+    {
+        Console.WriteLine("Input the width to print a '*' NorthEast triangle");
+
+        int height = Convert.ToInt32(Console.ReadLine()!);
+        int width = 0;
+        int max = height;
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                Console.Write(' ');
+            }
+
+            for (int k = 0; k < max; k++)
+            {
+                Console.Write('*');
+            }
+            Console.WriteLine();
+            width++;
+            max--;
+        }
+    }
     static void Triangle()
     {
         Console.WriteLine("Input the symbol and a width to print a triangle");
@@ -43,66 +105,18 @@
         }
     }
 
-    static void NETriangle()
+    static void CalculateFunction()
     {
-        Console.WriteLine("Input the width to print a '*' NorthEast triangle");
-
-        int height = Convert.ToInt32(Console.ReadLine()!);
-        int width = 0;
-        int max = height;
-
-        for (int i = 0; i < height; i++)
-        {
-            for(int j = 0; j < width; j++)
-            {
-                Console.Write(' ');
-            }
-
-            for(int k = 0; k < max; k++)
-            {
-                Console.Write('*');
-            }
-            Console.WriteLine();
-            width++;
-            max--;
-        }      
-    }
-
-    static void RightAlignTriangle()
-    {
-        Console.WriteLine("Input a string and it will make a right-aligned triangle");
-
-        string input = Console.ReadLine()!;        
-        int height = input.Length - 1;
-        int width = 1;
-
-        Console.WriteLine();
-        for(int i = height; i >= 0; i--)
-        {
-            Console.WriteLine(input.Substring(i, width));
-            width++;
-        }
-    }
-
-    static void GraphicFunction()
-    {
-        Console.WriteLine("Input two numbers for a range to calculate the following: y = (x - 4)^2");
+        Console.WriteLine("Input two numbers for a range to calculate the following: y = x^2 - 2x + 1");
 
         int x = Convert.ToInt32(Console.ReadLine());
         int y = Convert.ToInt32(Console.ReadLine());
 
         for (int i = x; i <= y; i++)
         {
-            double function = (i - 4);
-            double graphicFunc = Math.Pow(function, 2);
+            double function = Math.Pow(i, 2) - (2 * i) + 1;
 
-            for(int j = 0; j < graphicFunc; j++)
-            {
-                Console.Write('*');
-            }
-            Console.WriteLine();
+            Console.Write("{0} ", function);
         }
     }
-
-
 }
