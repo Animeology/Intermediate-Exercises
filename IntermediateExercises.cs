c431e9011270238e2ed7ebd9844cae316b148b4b
Animeology <joseph.nguyen19@yahoo.com>
2023-06-02 17:43:37 -0500

    access control lesson

--- IntermediateExercises.cs
+++ IntermediateExercises.cs
@@ -2,12 +2,44 @@
 {
     static void Main(string[] args)
     {
-        MultipleThreesAndFives();
+        AccessControl();
+        // MultipleThreesAndFives();
         // GraphicFunction();
         // RightAlignTriangle();
         // NETriangle();
         // Triangle();
         // CalculateFunction();
+    }
+
+    static void AccessControl()
+    {
+        Console.WriteLine("Try to login with the username and password");
+
+        int attempts = 0;
+
+        while (attempts < 3)
+        {
+            Console.Write("Username: ");
+            string username = Console.ReadLine()!;
+
+            Console.Write("Password: ");
+            string password = Console.ReadLine()!;
+
+            if (username == "12" && password == "1234")
+            {
+                Console.WriteLine("Login Successful!");
+            }
+            else
+            {
+                Console.WriteLine("Login Failed!");
+            }
+            attempts++;
+
+            if(attempts == 3)
+            {
+                Console.WriteLine("You used all your attempts!");
+            }
+        }
     }
 
     static void MultipleThreesAndFives()
