e3d1cf825337e2ce2525e4caf147d9e17916fbf1
Animeology <joseph.nguyen19@yahoo.com>
2023-06-02 17:30:59 -0500

    graphic function lesson

--- IntermediateExercises.cs
+++ IntermediateExercises.cs
@@ -2,7 +2,8 @@
 {
     static void Main(string[] args)
     {
-        RightAlignTriangle();
+        GraphicFunction();
+        // RightAlignTriangle();
         // NETriangle();
         // Triangle();
         // CalculateFunction();
@@ -82,4 +83,26 @@
             width++;
         }
     }
+
+    static void GraphicFunction()
+    {
+        Console.WriteLine("Input two numbers for a range to calculate the following: y = (x - 4)^2");
+
+        int x = Convert.ToInt32(Console.ReadLine());
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = x; i <= y; i++)
+        {
+            double function = (i - 4);
+            double graphicFunc = Math.Pow(function, 2);
+
+            for(int j = 0; j < graphicFunc; j++)
+            {
+                Console.Write('*');
+            }
+            Console.WriteLine();
+        }
+    }
+
+
 }
