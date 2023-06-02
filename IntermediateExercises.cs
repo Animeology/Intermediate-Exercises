966d46c17028b2184cf70adb940642f86f929dd8
Animeology <joseph.nguyen19@yahoo.com>
2023-06-02 14:30:06 -0500

    NorthEast Triangle lesson

--- IntermediateExercises.cs
+++ IntermediateExercises.cs
@@ -2,7 +2,8 @@
 {
     static void Main(string[] args)
     {
-        Triangle();
+        NETriangle();
+        // Triangle();
         // CalculateFunction();
     }
 
@@ -38,6 +39,31 @@
             Console.WriteLine();
             width--;
         }
+    }
 
+    static void NETriangle()
+    {
+        Console.WriteLine("Input the width to print a '*' NorthEast triangle");
+
+        int height = Convert.ToInt32(Console.ReadLine()!);
+        int width = 0;
+        int max = height;
+
+        for (int i = 0; i < height; i++)
+        {
+            for(int j = 0; j < width; j++)
+            {
+                Console.Write(' ');
+            }
+
+            for(int k = 0; k < max; k++)
+            {
+                Console.Write('*');
+            }
+            Console.WriteLine();
+            width++;
+            max--;
+        }
+        
     }
 }
