62353c37e6dca7b5ed1b9e89585cb3e07e2f0108
Animeology <joseph.nguyen19@yahoo.com>
2023-06-03 13:42:52 -0500

    RepetitiveStructures lesson

--- IntermediateExercises.cs
+++ IntermediateExercises.cs
@@ -2,7 +2,8 @@
 {
     static void Main(string[] args)
     {
-        InfiniteDivide();
+        RepetitiveStructures();
+        // InfiniteDivide();
         // AccessControl();
         // MultipleThreesAndFives();
         // GraphicFunction();
@@ -10,6 +11,40 @@
         // NETriangle();
         // Triangle();
         // CalculateFunction();
+    }
+
+    static void RepetitiveStructures()
+    {
+        Console.WriteLine("Input two numbers and it will show the range between these two numbers included the two inputs");
+
+        int x = Convert.ToInt32(Console.ReadLine());
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        // For Loop
+        for(int i = x; i <= y; i++)
+        {
+            Console.Write("{0} ", i);
+        }
+
+        Console.WriteLine();
+
+        // While loop
+        int loop = x;
+        while(loop <= y)
+        {
+            Console.Write("{0} ", loop);
+            loop++;
+        }
+
+        Console.WriteLine();
+
+        // Do While loop
+        int j = x;
+        do
+        {
+            Console.Write("{0} ", j);
+            j++;
+        } while (j <= y);
     }
 
     static void InfiniteDivide()
