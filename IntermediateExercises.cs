8f0a05a66385e503b9fa463efd18e6659fbbcd94
Animeology <joseph.nguyen19@yahoo.com>
2023-06-03 14:03:53 -0500

    math statistics lesson

--- IntermediateExercises.cs
+++ IntermediateExercises.cs
@@ -2,15 +2,56 @@
 {
     static void Main(string[] args)
     {
-        RepetitiveStructures();
-        // InfiniteDivide();
-        // AccessControl();
-        // MultipleThreesAndFives();
-        // GraphicFunction();
-        // RightAlignTriangle();
-        // NETriangle();
-        // Triangle();
-        // CalculateFunction();
+        MathStatistics();
+        //RepetitiveStructures();
+        //InfiniteDivide();
+        //AccessControl();
+        //MultipleThreesAndFives();
+        //GraphicFunction();
+        //RightAlignTriangle();
+        //NETriangle();
+        //Triangle();
+        //CalculateFunction();
+    }
+
+    static void MathStatistics()
+    {
+        Console.WriteLine("Input 5 numbers and it will calculate the sum, mean, the max, and the minimum number");
+
+        int totalNums = 5;
+        int[] nums = new int[totalNums];
+
+        for(int i = 0; i < totalNums; i++)
+        {
+            nums[i] = int.Parse(Console.ReadLine()!);
+        }
+
+        int sum = 0;
+        int max = int.MinValue;
+        int min = int.MaxValue;
+
+        for(int i = 0; i < totalNums; i++)
+        {
+            sum += nums[i];
+
+            if (nums[i] > max)
+            {
+                max = nums[i];
+            }
+
+            if (nums[i] < min)
+            {
+                min = nums[i];
+            }
+
+        }
+       
+        int mean = sum / totalNums;
+
+        Console.WriteLine("Sum: {0}", sum);
+        Console.WriteLine("Mean: {0}", mean);
+        Console.WriteLine("Max: {0}", max);
+        Console.WriteLine("Min: {0}", min);
     }
 
     static void RepetitiveStructures()
