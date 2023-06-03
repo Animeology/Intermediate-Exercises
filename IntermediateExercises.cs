9920b9114f4bc61de4232eaab9c6ee997c4cc3cb
Animeology <joseph.nguyen19@yahoo.com>
2023-06-03 14:23:22 -0500

    money change lesson

--- IntermediateExercises.cs
+++ IntermediateExercises.cs
@@ -2,7 +2,8 @@
 {
     static void Main(string[] args)
     {
-        Alphabet();
+        MoneyChange();
+        //Alphabet();
         //MathStatistics();
         //RepetitiveStructures();
         //InfiniteDivide();
@@ -13,6 +14,53 @@
         //NETriangle();
         //Triangle();
         //CalculateFunction();
+    }
+
+    static void MoneyChange()
+    {
+        Console.WriteLine("Request the total money in the cash register and the purchase value");
+
+        int total = Convert.ToInt32(Console.ReadLine());
+        int value = Convert.ToInt32(Console.ReadLine());
+
+        int change = total - value;
+
+        while(change >= 50)
+        {
+            Console.WriteLine("50 ");
+            change -= 50;
+        }
+
+        while (change >= 20)
+        {
+            Console.WriteLine("20 ");
+            change -= 20;
+        }
+
+        while (change >= 10)
+        {
+            Console.WriteLine("10 ");
+            change -= 10;
+        }
+
+        while (change >= 5)
+        {
+            Console.WriteLine("5 ");
+            change -= 5;
+        }
+
+        while (change >= 2)
+        {
+            Console.WriteLine("2 ");
+            change -= 2;
+        }
+
+        while (change >= 1)
+        {
+            Console.WriteLine("1 ");
+            change -= 1;
+        }
+
     }
 
     static void Alphabet()
