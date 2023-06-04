eee8976498438a868ec9fff58f477fd157677609
Animeology <joseph.nguyen19@yahoo.com>
2023-06-03 19:21:49 -0500

    conditional operator lesson

--- IntermediateExercises.cs
+++ IntermediateExercises.cs
@@ -2,7 +2,8 @@
 {
     static void Main(string[] args)
     {
-        MoneyChange();
+        ConditionalOp();
+        //MoneyChange();
         //Alphabet();
         //MathStatistics();
         //RepetitiveStructures();
@@ -14,6 +15,23 @@
         //NETriangle();
         //Triangle();
         //CalculateFunction();
+    }
+
+    static void ConditionalOp()
+    {
+        Console.WriteLine("Input two numbers and it will output whether they are positive");
+
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        var num1Pos = a > 0 ? "A is positive" : "A is negative";
+        var num2Pos = b > 0 ? "B is positive" : "B is negative";
+        var bothPos = a > 0 && b > 0 ? "Both are positive" : "One of them is negative or Both are negative";
+
+        Console.WriteLine(num1Pos);
+        Console.WriteLine(num2Pos);
+        Console.WriteLine(bothPos);
+
     }
 
     static void MoneyChange()
