3a3c79854a69f9afb10ac1818e0f48dcf4586341
Animeology <joseph.nguyen19@yahoo.com>
2023-06-03 19:33:23 -0500

    nested conditional operator lesson

--- IntermediateExercises.cs
+++ IntermediateExercises.cs
@@ -2,7 +2,8 @@
 {
     static void Main(string[] args)
     {
-        ConditionalOp();
+        NestedConditionalOp();
+        //ConditionalOp();
         //MoneyChange();
         //Alphabet();
         //MathStatistics();
@@ -15,6 +16,18 @@
         //NETriangle();
         //Triangle();
         //CalculateFunction();
+    }
+
+    static void NestedConditionalOp()
+    {
+        Console.WriteLine("Input two numbers and it will check how many of them are positive");
+
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        var pos = (a > 0 && b > 0) ? 2 : ((a > 0 || b > 0) ? 1 : 0);
+
+        Console.WriteLine(pos);
     }
 
     static void ConditionalOp()
