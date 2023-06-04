cda802ca89691dfcc07a360b7f0f5bc81de433c8
Animeology <joseph.nguyen19@yahoo.com>
2023-06-04 12:40:31 -0500

    hollow rectangle lesson

--- IntermediateExercises.cs
+++ IntermediateExercises.cs
@@ -2,7 +2,8 @@
 {
     static void Main(string[] args)
     {
-        HollowSquare();
+        HollowRectangle();
+        //HollowSquare();
         //NestedConditionalOp();
         //ConditionalOp();
         //MoneyChange();
@@ -19,25 +20,26 @@
         //CalculateFunction();
     }
 
-    static void HollowSquare()
+    static void HollowRectangle()
     {
         Console.WriteLine("Input a symbol and the width/height to display a hollow square");
 
         char symbol = Convert.ToChar(Console.ReadLine()!);
-        int size = Convert.ToInt32(Console.ReadLine());
+        int width = Convert.ToInt32(Console.ReadLine());
+        int height = Convert.ToInt32(Console.ReadLine());
 
-        for(int i = 0; i < size; i++)
+        for (int i = 0; i < width; i++)
         {
             Console.Write(symbol);
         }
 
         Console.WriteLine();
 
-        for(int i = 0; i < size - 2; i++) 
+        for (int i = 0; i < height; i++)
         {
             Console.Write(symbol);
 
-            for(int j = 0; j < size - 2; j++)
+            for(int j = 0; j < width - 2; j++)
             {
                 Console.Write(" ");
             }
@@ -45,7 +47,42 @@
             Console.WriteLine(symbol);
         }
 
-        for(int i = 0; i < size; i++)
+        for (int i = 0; i < width; i++)
+        {
+            Console.Write(symbol);
+        }
+
+        Console.WriteLine();
+
+    }
+
+    static void HollowSquare()
+    {
+        Console.WriteLine("Input a symbol and the width/height to display a hollow square");
+
+        char symbol = Convert.ToChar(Console.ReadLine()!);
+        int size = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write(symbol);
+        }
+
+        Console.WriteLine();
+
+        for (int i = 0; i < size - 2; i++)
+        {
+            Console.Write(symbol);
+
+            for (int j = 0; j < size - 2; j++)
+            {
+                Console.Write(" ");
+            }
+
+            Console.WriteLine(symbol);
+        }
+
+        for (int i = 0; i < size; i++)
         {
             Console.Write(symbol);
         }
