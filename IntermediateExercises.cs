6b82a120c30ac766bdd8636ea79a5f102faeda2a
Animeology <joseph.nguyen19@yahoo.com>
2023-06-04 15:44:24 -0500

    hexadecimal table lesson

--- IntermediateExercises.cs
+++ IntermediateExercises.cs
@@ -2,7 +2,8 @@
 {
     static void Main(string[] args)
     {
-        BinaryHexadecimal();
+        HexadecimalTable();
+        //BinaryHexadecimal();
         //PrimeFactors();
         //Pyramid();
         //Parallelogram();
@@ -22,6 +23,28 @@
         //NETriangle();
         //Triangle();
         //CalculateFunction();
+    }
+
+    static void HexadecimalTable()
+    {
+        Console.WriteLine("Prints a hexadecimal table");
+
+        int max = 255;
+
+        for(int i = 0; i <= max; i++)
+        {
+            if(i < 16)
+            {
+                Console.Write("0");
+            }
+
+            Console.Write("{0} ", Convert.ToString(i, 16));
+
+            if(i % 16 == 15)
+            {
+                Console.WriteLine();
+            }
+        }
     }
 
     static void BinaryHexadecimal()
