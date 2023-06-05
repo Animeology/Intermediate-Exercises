317bf87ebc53c69920035325590d1a561da2e445
Animeology <joseph.nguyen19@yahoo.com>
2023-06-05 14:06:57 -0500

    array search lesson

--- IntermediateExercises.cs
+++ IntermediateExercises.cs
@@ -2,7 +2,8 @@
 {
     static void Main(string[] args)
     {
-        ReverseArray();
+        SearchArray();
+        //ReverseArray();
         //HexadecimalTable();
         //BinaryHexadecimal();
         //PrimeFactors();
@@ -26,6 +27,36 @@
         //CalculateFunction();
     }
 
+    static void SearchArray()
+    {
+        Console.WriteLine("Input 5 numbers and input the number to search through the array");
+
+        int count = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        
+        Console.WriteLine("What number do you want to search?");
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        for (int j = 0; j < count; j++)
+        {
+            if (arr[j] == y)
+            {
+                Console.WriteLine("The number {0} exists", y);
+                break;
+            }
+            else
+            {
+                Console.WriteLine("The number {0} doesn't exist", y);
+                break;
+            }
+        }
+    }
+
     static void ReverseArray()
     {
         Console.WriteLine("Input 5 numbers and it will print it in reverse array");
@@ -40,7 +71,7 @@
 
         Array.Reverse(nums);
 
-        for(int i = 0; i < max; i++)
+        for (int i = 0; i < max; i++)
         {
             Console.Write("{0} ", nums[i]);
         }
