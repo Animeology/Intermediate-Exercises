d5198b96fb976807aa2f16d3176e11ad1dda2ac7
Animeology <joseph.nguyen19@yahoo.com>
2023-06-06 18:06:59 -0500

    2D Character Array lesson

--- IntermediateExercises.cs
+++ IntermediateExercises.cs
@@ -2,7 +2,8 @@
 {
     static void Main(string[] args)
     {
-        TwoDeeArrays();
+        TwoDeeCharArray();
+        //TwoDeeArrays();
         //BubbleSort();
         //SearchPosAndNeg();
         //SearchEvenNums();
@@ -29,6 +30,27 @@
         //NETriangle();
         //Triangle();
         //CalculateFunction();
+    }
+
+    static void TwoDeeCharArray()
+    {
+        Console.WriteLine("Draws a 20(row) by 70(col) of 'X'");
+
+        int rows = 20;
+        int cols = 70;
+        char x = 'X';
+
+        char[,] arr = new char[rows, cols];        
+
+        for(int i = 0; i < rows; i++)
+        {
+            for(int j = 0; j < cols; j++)
+            {
+                arr[i, j] = x;
+                Console.Write(arr[i, j]);
+            }
+            Console.WriteLine();
+        }
     }
 
     static void TwoDeeArrays()
