10bb6532ca5488ef515a73c1014f82cd0f102577
Animeology <joseph.nguyen19@yahoo.com>
2023-06-06 14:46:03 -0500

    bubble sort lesson

--- IntermediateExercises.cs
+++ IntermediateExercises.cs
@@ -2,7 +2,8 @@
 {
     static void Main(string[] args)
     {
-        SearchPosAndNeg();
+        BubbleSort();
+        //SearchPosAndNeg();
         //SearchEvenNums();
         //SearchArray();
         //ReverseArray();
@@ -27,6 +28,39 @@
         //NETriangle();
         //Triangle();
         //CalculateFunction();
+    }
+
+    static void BubbleSort()
+    {
+        Console.WriteLine("Input 10 numbers and it will order them using Bubble Sort");
+
+        int count = 10;
+        int[] nums = new int[count];
+
+        for(int i = 0; i < count; i++)
+        {
+            nums[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        int temp;
+        for(int i =  0; i <= nums.Length - 2; i++) 
+        {
+            for(int j = 0; j <= nums.Length - 2; j++)
+            {
+                if (nums[j] > nums[j + 1])
+                {
+                    temp = nums[j + 1];
+                    nums[j + 1] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            Console.Write(nums[i]);
+        }
+
     }
 
     static void SearchPosAndNeg()
