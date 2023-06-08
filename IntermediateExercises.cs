0ed2ef8fdb95c8f18b6c02be7add4cf4e201d9a6
Animeology <joseph.nguyen19@yahoo.com>
2023-06-08 14:34:54 -0500

    Power Operation lesson

--- IntermediateExercises.cs
+++ IntermediateExercises.cs
@@ -5,7 +5,8 @@ class IntermediateExercises
 {
     static void Main(string[] args)
     {
-        GreatestValue();
+        PowerOperation();
+        //GreatestValue();
         //ReverseString();
         //Add();
         //ModifyCharacter();
@@ -40,6 +41,23 @@ class IntermediateExercises
         //NETriangle();
         //Triangle();
         //CalculateFunction();
+    }
+
+    static void PowerOperation()
+    {
+        Console.WriteLine("Input 2 numbers and the it will calculate the first number of the power of the second number");
+
+        double num = Convert.ToInt32(Console.ReadLine());
+        double power = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(Power(num, power));
+
+        double Power(double num, double power)
+        {
+            double result = Math.Pow(num, power);
+
+            return result;
+        }
     }
 
     static void GreatestValue()
