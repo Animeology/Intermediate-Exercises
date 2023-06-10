ced0f533c8fccfb90a32f158878446a81e71bac6
Animeology <joseph.nguyen19@yahoo.com>
2023-06-10 13:23:43 -0500

    main parameter calculator return lesson

--- IntermediateExercises.cs
+++ IntermediateExercises.cs
@@ -2,7 +2,8 @@
 {
     public static void Main(string[] args)
     {
-        MainParameterCalculator(args);
+        MainParameterCalculatorReturn(args);
+        //MainParameterCalculator(args);
         //CheckNumber();
         //CheckAlphabet();
         //SwapParameters();
@@ -47,10 +48,63 @@
         //RightAlignTriangle();
         //NETriangle();
         //Triangle();
-        //CalculateFunction();
+        //CalculateFunction();        
     }
 
-    private static void MainParameterCalculator(string[] args)
+    static int MainParameterCalculatorReturn(string[] args)
+    {
+        Console.WriteLine("Input an equation and it will calculate");
+        Console.WriteLine("Accepts only \"+\", \"-\", \"*\" \"x\" \"X\", \"/\" ");
+
+        int x = Convert.ToInt32(args[0]);
+        char op = Convert.ToChar(args[1]);
+        int y = Convert.ToInt32(args[2]);
+
+        if(args.Length != 0)
+        {
+            return 1;
+        }
+
+        if (args[1] != "+" || args[1] != "-" || args[1] != "*" || args[1] != "/")
+        {
+            return 2;
+        }
+
+        try
+        {
+            Convert.ToInt32(x);
+            Convert.ToInt32(y);
+        }
+        catch 
+        {
+            return 3;
+        }
+
+        switch (op)
+        {
+            case '+':
+                Console.Write(x + y);
+                break;
+            case '-':
+                Console.Write(x - y);
+                break;
+            case '*':
+            case 'x':
+            case 'X':
+                Console.Write(x * y);
+                break;
+            case '/':
+                Console.Write(x / y);
+                break;
+            default:
+                Console.WriteLine("Invalid operation");
+                break;
+        }
+
+        return 0;
+    }
+
+    static void MainParameterCalculator(string[] args)
     {
         Console.WriteLine("Input an equation and it will calculate");
         Console.WriteLine("Accepts only \"+\", \"-\", \"*\" \"x\" \"X\", \"/\" ");
