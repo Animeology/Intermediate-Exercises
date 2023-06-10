d503baaeadd6f25f7b13e46c94094836f7b27949
Animeology <joseph.nguyen19@yahoo.com>
2023-06-10 13:10:56 -0500

    Main parameter calculator lesson

--- IntermediateExercises.cs
+++ IntermediateExercises.cs
@@ -1,8 +1,36 @@
 ﻿class IntermediateExercises
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        CheckNumber();
+        Console.WriteLine("Input an equation and it will calculate");
+        Console.WriteLine("Accepts only \"+\", \"-\", \"*\" \"x\" \"X\", \"/\" ");
+
+        int x = Convert.ToInt32(args[0]);
+        char op = Convert.ToChar(args[1]);
+        int y = Convert.ToInt32(args[2]);
+
+        switch (op)
+        {
+            case '+':
+                Console.Write(x + y);
+                break;
+            case '-':
+                Console.Write(x - y);
+                break;
+            case '*':
+            case 'x':
+            case 'X':
+                Console.Write(x * y);
+                break;
+            case '/':
+                Console.Write(x / y);
+                break;
+            default:
+                Console.WriteLine("Invalid operation");
+                break;
+        }
+
+        //CheckNumber();
         //CheckAlphabet();
         //SwapParameters();
         //CalculateRefDouble();
@@ -70,7 +98,6 @@
         }
     }
 
-
     static void CheckAlphabet()
     {
         Console.WriteLine("Input a string to check if the whole string is only alphabetic");
@@ -83,7 +110,7 @@
         {
             text = text.ToUpper();
 
-            for(int i = 0; i < text.Length; i++)
+            for (int i = 0; i < text.Length; i++)
             {
                 if (text[i] < 'A' || text[i] > 'Z')
                 {
@@ -1252,6 +1279,7 @@
             max--;
         }
     }
+
     static void Triangle()
     {
         Console.WriteLine("Input the symbol and a width to print a triangle");
