bbda8801b7a70cd7d6d6417550bb682bc7b500f9
Animeology <joseph.nguyen19@yahoo.com>
2023-06-10 13:13:37 -0500

    Extract the previous method into a method

--- IntermediateExercises.cs
+++ IntermediateExercises.cs
@@ -2,34 +2,7 @@
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Input an equation and it will calculate");
-        Console.WriteLine("Accepts only \"+\", \"-\", \"*\" \"x\" \"X\", \"/\" ");
-
-        int x = Convert.ToInt32(args[0]);
-        char op = Convert.ToChar(args[1]);
-        int y = Convert.ToInt32(args[2]);
-
-        switch (op)
-        {
-            case '+':
-                Console.Write(x + y);
-                break;
-            case '-':
-                Console.Write(x - y);
-                break;
-            case '*':
-            case 'x':
-            case 'X':
-                Console.Write(x * y);
-                break;
-            case '/':
-                Console.Write(x / y);
-                break;
-            default:
-                Console.WriteLine("Invalid operation");
-                break;
-        }
-
+        MainParameterCalculator(args);
         //CheckNumber();
         //CheckAlphabet();
         //SwapParameters();
@@ -75,6 +48,37 @@
         //NETriangle();
         //Triangle();
         //CalculateFunction();
+    }
+
+    private static void MainParameterCalculator(string[] args)
+    {
+        Console.WriteLine("Input an equation and it will calculate");
+        Console.WriteLine("Accepts only \"+\", \"-\", \"*\" \"x\" \"X\", \"/\" ");
+
+        int x = Convert.ToInt32(args[0]);
+        char op = Convert.ToChar(args[1]);
+        int y = Convert.ToInt32(args[2]);
+
+        switch (op)
+        {
+            case '+':
+                Console.Write(x + y);
+                break;
+            case '-':
+                Console.Write(x - y);
+                break;
+            case '*':
+            case 'x':
+            case 'X':
+                Console.Write(x * y);
+                break;
+            case '/':
+                Console.Write(x / y);
+                break;
+            default:
+                Console.WriteLine("Invalid operation");
+                break;
+        }
     }
 
     static void CheckNumber()
