04662c7fe61b1b281f5de133f25902117e9deb67
Animeology <joseph.nguyen19@yahoo.com>
2023-06-10 13:29:56 -0500

    Find Min/Max in array lesson

--- IntermediateExercises.cs
+++ IntermediateExercises.cs
@@ -2,7 +2,8 @@
 {
     public static void Main(string[] args)
     {
-        MainParameterCalculatorReturn(args);
+        MaxMinArray();
+        //MainParameterCalculatorReturn(args);
         //MainParameterCalculator(args);
         //CheckNumber();
         //CheckAlphabet();
@@ -49,6 +50,41 @@
         //NETriangle();
         //Triangle();
         //CalculateFunction();        
+    }
+
+    static void MaxMinArray()
+    {
+        Console.WriteLine("Input 5 numbers into the array and it will find the max and min number in the array");
+
+        int count = 5;
+        int[] arr = new int[count];
+
+        int max = int.MinValue;
+        int min = int.MaxValue;
+
+        for(int i = 0; i < count; i++)
+        {
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        FindMinMax(arr, max, min);
+
+        void FindMinMax(int[] arr, int max, int min)
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+            Console.WriteLine("Min: {0}", min);
+            Console.WriteLine("Max: {0}", max);
+        }
     }
 
     static int MainParameterCalculatorReturn(string[] args)
