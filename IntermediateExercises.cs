eae7299e08746d76b1ee81cbb0b6f0bd46b071cf
Animeology <joseph.nguyen19@yahoo.com>
2023-06-10 12:42:54 -0500

    check alphabet lesson

--- IntermediateExercises.cs
+++ IntermediateExercises.cs
@@ -2,7 +2,8 @@
 {
     static void Main(string[] args)
     {
-        SwapParameters();
+        CheckAlphabet();
+        //SwapParameters();
         //CalculateRefDouble();
         //CalculateDouble();
         //AddArray();
@@ -45,6 +46,30 @@
         //NETriangle();
         //Triangle();
         //CalculateFunction();
+    }
+
+    static void CheckAlphabet()
+    {
+        Console.WriteLine("Input a string to check if the whole string is only alphabetic");
+
+        string text = Console.ReadLine()!;
+
+        Console.WriteLine(IsAlphabetic(text));
+
+        bool IsAlphabetic(string text)
+        {
+            text = text.ToUpper();
+
+            for(int i = 0; i < text.Length; i++)
+            {
+                if (text[i] < 'A' || text[i] > 'Z')
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 
     static void SwapParameters()
