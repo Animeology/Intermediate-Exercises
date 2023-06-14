44221bd4d7982a6019334cbdacbc781e3beea938
Animeology <joseph.nguyen19@yahoo.com>
2023-06-14 13:13:18 -0500

    reverse file lesson

--- IntermediateExercises.cs
+++ IntermediateExercises.cs
@@ -4,7 +4,8 @@ class IntermediateExercises
 {
     public static void Main(string[] args)
     {
-        ChangeLowerToUpperFile();
+        ReverseFile();
+        //ChangeLowerToUpperFile();
         //CountWordsInFile();
         //SearchLinesInFile();
         //PalindromeRecursion();
@@ -70,6 +71,27 @@ class IntermediateExercises
         //Triangle();
         //CalculateFunction();
     }
+
+    static void ReverseFile()
+    {
+        Console.WriteLine("Creates a new file that reverse every word in the input.txt file");
+        string file = "input.txt";
+        string newFile = "input.inv";
+
+        using (StreamReader sr = new StreamReader(file))
+        {
+            using (StreamWriter sw = new StreamWriter(newFile))
+            {
+                string[] lines = File.ReadAllLines(file);
+
+                for(int i = lines.Length - 1; i >= 0; i--)
+                {
+                    sw.WriteLine(lines[i]);
+                }
+            }
+        }
+    }
+
 
     static void ChangeLowerToUpperFile()
     {
