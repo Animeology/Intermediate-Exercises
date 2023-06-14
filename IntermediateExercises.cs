bd1797a738152fdd0a53cce71bd231c5393245fe
Animeology <joseph.nguyen19@yahoo.com>
2023-06-14 11:10:20 -0500

    Search in file lesson

--- IntermediateExercises.cs
+++ IntermediateExercises.cs
@@ -4,6 +4,7 @@ class IntermediateExercises
 {
     public static void Main(string[] args)
     {
+        SearchLinesInFile();
         //PalindromeRecursion();
         //ReverseRecursion();
         //FactorialRecursion();
@@ -66,6 +67,34 @@ class IntermediateExercises
         //NETriangle();
         //Triangle();
         //CalculateFunction();
+    }
+
+    static void SearchLinesInFile()
+    {
+        Console.WriteLine("Search for the word in the file and it will print out the line that the word is in");
+        string file = "input.txt";
+        string[] lines = File.ReadAllLines(file);
+
+        bool exit = false;
+        do
+        {
+            string text = Console.ReadLine()!;
+
+            if (text != string.Empty)
+            {
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    if (lines[i].Contains(text))
+                    {
+                        Console.WriteLine(lines[i]);
+                    }
+                }
+            }
+            else
+            {
+                exit = true;
+            }
+        } while (!exit);
     }
 
     static void PalindromeRecursion()
