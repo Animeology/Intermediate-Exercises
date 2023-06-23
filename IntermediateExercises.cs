447da36f16c906fd0d43b326652d6ad31dea3956
Animeology <joseph.nguyen19@yahoo.com>
2023-06-23 17:38:50 -0500

    read bmp lesson

--- IntermediateExercises.cs
+++ IntermediateExercises.cs
@@ -1,10 +1,12 @@
 ﻿using System.Text;
+using System.IO;
 
 class IntermediateExercises
 {
     public static void Main(string[] args)
     {
-        ReadID3();
+        BMPImage();
+        //ReadID3();
         //ReverseFile();
         //ChangeLowerToUpperFile();
         //CountWordsInFile();
@@ -71,6 +73,32 @@ class IntermediateExercises
         //NETriangle();
         //Triangle();
         //CalculateFunction();
+    }
+
+    static void BMPImage()
+    {
+        string fileName = "logo.bmp";
+        int size = 54;
+        int width, height;
+        byte[] data = new byte[size];
+
+        using (FileStream file = File.OpenRead(fileName))
+        {
+            file.Read(data, 0, size);
+        }
+
+        width = data[18]
+                + (data[19] * 256)
+                + (data[20] * 256 * 256)
+                + (data[21] * 256 * 256 * 256);
+
+        height = data[22] 
+                + (data[23] * 256) 
+                + (data[24] * 256 * 256) 
+                + (data[25] * 256 * 256 * 256);
+
+
+        Console.WriteLine("{0}x{1}", width, height);
     }
 
     static void ReadID3()
