c2a5e12b9197ad5b52beaf070daaa8964a6a5464
Animeology <joseph.nguyen19@yahoo.com>
2023-06-23 18:09:11 -0500

    inverted byte file lesson

--- IntermediateExercises.cs
+++ IntermediateExercises.cs
@@ -5,7 +5,8 @@ class IntermediateExercises
 {
     public static void Main(string[] args)
     {
-        ReadBMP();
+        InvertedFile();
+        //ReadBMP();
         //BMPImage();
         //ReadID3();
         //ReverseFile();
@@ -74,6 +75,28 @@ class IntermediateExercises
         //NETriangle();
         //Triangle();
         //CalculateFunction();
+    }
+
+    static void InvertedFile()
+    {
+        string inputFileName = "app.exe";
+        string outputFileName = "app.inv";
+
+        using(FileStream file = File.OpenRead(inputFileName))
+        {
+            long size = file.Length;
+            byte[] data = new byte[size];
+
+            file.Read(data, 0, (int)size);
+
+            using (FileStream outFile = File.OpenWrite(outputFileName))
+            {
+                for(long i = 0; i < size; i++)
+                {
+                    outFile.WriteByte(data[i]);
+                }
+            }
+        }
     }
 
     static void ReadBMP()
