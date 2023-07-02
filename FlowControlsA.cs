19c331bbd64678f2406f6c59e0b224cec7c6c709
Animeology <joseph.nguyen19@yahoo.com>
2023-07-02 12:44:24 -0500

    Create FlowControlsA.cs

---
+++ FlowControlsA.cs
@@ -0,0 +1,46 @@
+﻿namespace Intermediate_Exercises
+{
+    public class FlowControlsA
+    {
+        public static void MathStatistics()
+        {
+            Console.WriteLine("Input 5 numbers and it will calculate the sum, mean, the max, and the minimum number");
+
+            int totalNums = 5;
+            int[] nums = new int[totalNums];
+
+            for (int i = 0; i < totalNums; i++)
+            {
+                nums[i] = int.Parse(Console.ReadLine()!);
+            }
+
+            int sum = 0;
+            int max = int.MinValue;
+            int min = int.MaxValue;
+
+            for (int i = 0; i < totalNums; i++)
+            {
+                sum += nums[i];
+
+                if (nums[i] > max)
+                {
+                    max = nums[i];
+                }
+
+                if (nums[i] < min)
+                {
+                    min = nums[i];
+                }
+
+            }
+
+            int mean = sum / totalNums;
+
+            Console.WriteLine("Sum: {0}", sum);
+            Console.WriteLine("Mean: {0}", mean);
+            Console.WriteLine("Max: {0}", max);
+            Console.WriteLine("Min: {0}", min);
+        }
+
+    }
+}
