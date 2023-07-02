c46ef0b93a7f2a2ccc205d4b64c5f39edd992391
Animeology <joseph.nguyen19@yahoo.com>
2023-07-02 12:17:38 -0500

    Create Arithmetic.cs

---
+++ Arithmetic.cs
@@ -0,0 +1,20 @@
+﻿namespace Intermediate_Exercises
+{
+    public class Arithmetic
+    {
+        static void CalculateFunction()
+        {
+            Console.WriteLine("Input two numbers for a range to calculate the following: y = x^2 - 2x + 1");
+
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = x; i <= y; i++)
+            {
+                double function = Math.Pow(i, 2) - (2 * i) + 1;
+
+                Console.Write("{0} ", function);
+            }
+        }
+    }
+}
