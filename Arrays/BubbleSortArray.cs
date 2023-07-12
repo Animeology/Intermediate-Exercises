ca4fecb15a800a16589071105c3050cad35f7b63
Animeology <joseph.nguyen19@yahoo.com>
2023-07-12 14:17:17 -0500

    Create BubbleSortArray.cs

---
+++ Arrays/BubbleSortArray.cs
@@ -0,0 +1,37 @@
+﻿namespace IntermediateExercises.Arrays
+{
+    public class BubbleSortArray
+    {
+        public static void BubbleSort()
+        {
+            Console.WriteLine("Input 10 numbers and it will order them using Bubble Sort");
+
+            int count = 10;
+            int[] nums = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                nums[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int temp;
+            for (int i = 0; i <= nums.Length - 2; i++)
+            {
+                for (int j = 0; j <= nums.Length - 2; j++)
+                {
+                    if (nums[j] > nums[j + 1])
+                    {
+                        temp = nums[j + 1];
+                        nums[j + 1] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write(nums[i]);
+            }
+        }
+    }
+}
