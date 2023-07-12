ddb8d9ffd3cd006b450fc2b44f889af13ad523d7
Animeology <joseph.nguyen19@yahoo.com>
2023-07-12 13:57:05 -0500

    Update IntermediateExercises.cs

--- IntermediateExercises.cs
+++ IntermediateExercises.cs
@@ -1,117 +1,18 @@
 ﻿namespace IntermediateExercises
 {
     using Arithmetic;
+    using Arrays;
 
     class IntermediateExercises
     {
         public static void Main(string[] args)
         {
+            // Arithmetic
             FunctionCalculator.CalculateFunction();
 
-            //Arrays arrays = new Arrays();
-            //arrays.Circumference();
-            //arrays.MathematicalStatistics();
-            //arrays.ArrayStruct();
-            //arrays.TwoDeeArrays();
-            //arrays.TwoDeeCharArray();
-            //arrays.BubbleSort();
-            //arrays.SearchPosAndNeg();
-            //arrays.SearchEvenNums();
-            //arrays.SearchArray();
-            //arrays.ReverseArray();
-
-            //BinaryFiles binaryFiles = new BinaryFiles();
-            //binaryFiles.InvertedFile();
-            //binaryFiles.ReadBMP();
-            //binaryFiles.BMPImage();
-            //binaryFiles.ReadID3();
-
-            //DataTypesB dataType = new DataTypesB();
-            //dataType.HexadecimalTable();
-            //dataType.BinaryHexadecimal();
-            //dataType.PrimeFactors();
-
-            //FlowControlsA flowControlsA = new FlowControlsA();
-            //flowControlsA.MathStatistics();
-
-            //FlowControlsB flowControlsB = new FlowControlsB();
-            //flowControlsB.RepetitiveStructures();
-            //flowControlsB.InfiniteDivide();
-            //flowControlsB.AccessControl();
-            //flowControlsB.MultipleThreesAndFives();
-
-            //FlowControlsC flowControlsC = new FlowControlsC();
-            //flowControlsC.NestedConditionalOp();
-            //flowControlsC.ConditionalOp();
-            //flowControlsC.MoneyChange();
-            //flowControlsC.Alphabet();
-
-            //FunctionsA functionsA = new FunctionsA();
-            //functionsA.SwapParameters();
-            //functionsA.CalculateRefDouble();
-            //functionsA.CalculateDouble();
-            //functionsA.AddArray();
-            //functionsA.FormatText();
-            //functionsA.SpaceFunction();
-
-            //FunctionsB functionsB = new FunctionsB();
-            //functionsB.Title();
-            //functionsB.PowerOperation();
-            //functionsB.GreatestValue();
-            //functionsB.ReverseString();
-            //functionsB.Add();
-            //functionsB.ModifyCharacter();
-
-            //FunctionsC functionsC = new FunctionsC();
-            //functionsC.Palindrome();
-            //functionsC.MaxMinArray();
-            //functionsC.MainParameterCalculatorReturn(args);
-            //functionsC.MainParameterCalculator(args);
-            //functionsC.CheckNumber();
-            //functionsC.CheckAlphabet();
-
-            //GeometryA geomA = new GeometryA();
-            //geomA.GraphicFunction();
-            //geomA.RightAlignTriangle();
-            //geomA.NETriangle();
-            //geomA.Triangle();
-
-            //GeometryB geomB = new GeometryB();
-            //geomB.Pyramid();
-            //geomB.Parallelogram();
-            //geomB.HollowRectangle();
-            //geomB.HollowSquare();
-
-            //OOP objectP = new OOP();
-            //objectP.AnimalEating();
-            //objectP.CarDriving();
-            //objectP.InheritanceObjects();
-            //objectP.PhotoBookTest();
-            //objectP.StudentTeacherTest();
-            //objectP.PersonNameConAndDes();
-            //objectP.PersonName();
-
-            //Recursion recursion = new Recursion();
-            //recursion.PalindromeRecursion();
-            //recursion.ReverseRecursion();
-            //recursion.FactorialRecursion();
-            //recursion.FibonacciRecursion();
-            //recursion.PowerRecursion();
-            //recursion.MultiplyRecursion();
-
-            //SQLIte sqLite = new SQLIte();
-            //sqLite.CreateDatabase();
-            //sqLite.ReadingListFromDB();
-            //sqLite.CreateMemoryDatabase();
-            //sqLite.CRUD();
-
-            //TextFilesA textFilesA = new TextFilesA();
-            //textFilesA.SearchLinesInFile();
-
-            //TextFilesB textFilesB = new TextFilesB();
-            //textFilesB.ReverseFile();
-            //textFilesB.ChangeLowerToUpperFile();
-            //textFilesB.CountWordsInFile();
+            // Arrays
+            ReverseArray.ReversingTheArray();
+            Circumference.CalculateCircumference();
         }
     }
 }
