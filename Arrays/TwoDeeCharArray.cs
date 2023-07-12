f213e87ea4cae59dd0820caa623a05a9335b3e34
Animeology <joseph.nguyen19@yahoo.com>
2023-07-12 14:17:26 -0500

    Create TwoDeeCharArray.cs

---
+++ Arrays/TwoDeeCharArray.cs
@@ -0,0 +1,26 @@
+﻿namespace IntermediateExercises.Arrays
+{
+    public class TwoDeeCharArray
+    {
+        public static void DrawTwoDeeCharArray()
+        {
+            Console.WriteLine("Draws a 20(row) by 70(col) of 'X'");
+
+            int rows = 20;
+            int cols = 70;
+            char x = 'X';
+
+            char[,] arr = new char[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    arr[i, j] = x;
+                    Console.Write(arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
