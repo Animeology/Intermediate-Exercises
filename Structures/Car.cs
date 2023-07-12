efcc9e85e0605e2ed9c251024a39ccd1363694d4
Animeology <joseph.nguyen19@yahoo.com>
2023-07-12 14:17:28 -0500

    Create Car.cs

---
+++ Structures/Car.cs
@@ -0,0 +1,14 @@
+﻿namespace IntermediateExercises.Structures
+{
+    public struct Car
+    {
+        public string model;
+        public int year;
+
+        public void SetCar()
+        {
+            model = Console.ReadLine()!;
+            year = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
