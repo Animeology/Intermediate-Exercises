df89d2a232fcd423ffaa3af09d1957d989446cd5
Animeology <joseph.nguyen19@yahoo.com>
2023-07-12 13:57:02 -0500

    Create ReverseArray.cs

---
+++ Arrays/ReverseArray.cs
@@ -0,0 +1,25 @@
+﻿namespace IntermediateExercises.Arrays
+{
+    public class ReverseArray
+    {
+        public static void ReversingTheArray()
+        {
+            Console.WriteLine("Input 5 numbers and it will print it in reverse array");
+
+            int max = 5;
+            int[] nums = new int[max];
+
+            for (int i = 0; i < max; i++)
+            {
+                nums[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Array.Reverse(nums);
+
+            for (int i = 0; i < max; i++)
+            {
+                Console.Write("{0} ", nums[i]);
+            }
+        }
+    }
+}
