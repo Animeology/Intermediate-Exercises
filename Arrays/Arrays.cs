77bfe8322c7075051222d5678b3742eef3d14906
Animeology <joseph.nguyen19@yahoo.com>
2023-07-12 13:56:56 -0500

    rename

--- Arrays/Arrays.cs
+++ Arrays/Arrays.cs
@@ -1,28 +1,7 @@
-﻿namespace Intermediate_Exercises
+﻿namespace IntermediateExercises.Arrays
 {
     public class Arrays
     {
-        public void Circumference()
-        {
-            Console.WriteLine("Draw the circumference of a oval");
-
-            double x = 0;
-            double y = 0;
-
-            for (int i = 0; i < 360; i++)
-            {
-                double radians = i * Math.PI / 180.0;
-
-                x = 35 + 8 * Math.Cos(radians);
-                y = 10 + 8 * Math.Sin(radians);
-
-                Console.SetCursorPosition((int)x, (int)y);
-                Console.Write("X");
-            }
-
-            Console.SetCursorPosition(1, 20);
-        }
-
         public void MathematicalStatistics()
         {
             int choice = 0;
