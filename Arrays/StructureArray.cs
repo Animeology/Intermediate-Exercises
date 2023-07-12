3aa7dd12d93aaa33b7e5b514f59323e22b156785
Animeology <joseph.nguyen19@yahoo.com>
2023-07-12 14:17:31 -0500

    Create StructureArray.cs

---
+++ Arrays/StructureArray.cs
@@ -0,0 +1,36 @@
+﻿using IntermediateExercises.Structures;
+
+namespace IntermediateExercises.Arrays
+{
+    public class StructureArray
+    {
+        public static void ArrayStruct()
+        {
+            Console.WriteLine("Input 3 car models and the year of production for each and it will display them in order of production");
+
+            int totalCars = 3;
+            Car[] cars = new Car[totalCars];
+
+            for (int i = 0; i < totalCars; i++)
+            {
+                cars[i].SetCar();
+            }
+
+            for (int i = 0; i < totalCars - 2; i++)
+            {
+                Car temp;
+                if (cars[i].year > cars[i + 1].year)
+                {
+                    temp = cars[i];
+                    cars[i] = cars[i + 1];
+                    cars[i + 1] = temp;
+                }
+            }
+
+            for (int i = 0; i < totalCars; i++)
+            {
+                Console.WriteLine("Model: {0}, Year of production: {1}", cars[i].model, cars[i].year);
+            }
+        }
+    }
+}
