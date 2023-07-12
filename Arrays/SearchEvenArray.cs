11b6d95bb26d31554e0284e96664bf61fe0ce925
Animeology <joseph.nguyen19@yahoo.com>
2023-07-12 14:17:12 -0500

    Create SearchEvenArray.cs

---
+++ Arrays/SearchEvenArray.cs
@@ -0,0 +1,28 @@
+﻿namespace IntermediateExercises.Arrays
+{
+    public class SearchEvenArray
+    {
+        public static void SearchEvenNums()
+        {
+            Console.WriteLine("Input 10 numbers and it will display only the even numbers");
+
+            int count = 10;
+            int[] arr = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Even Numbers:");
+
+            for (int i = 0; i < count; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+        }
+    }
+}
