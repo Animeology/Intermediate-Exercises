733a7d4d562951e93f8aedef3a60a39b7a6a4ccb
Animeology <joseph.nguyen19@yahoo.com>
2023-07-13 15:44:42 -0500

    Create CountSpaces.cs

---
+++ FunctionsA/CountSpaces.cs
@@ -0,0 +1,29 @@
+﻿namespace IntermediateExercises.FunctionsA
+{
+    public class CountSpaces
+    {
+        public static void SpaceFunction()
+        {
+            Console.WriteLine("Input a sentence and it will count the total spaces in the sentence");
+
+            string text = Console.ReadLine()!;
+
+            Console.WriteLine(CountSpaces(text));
+
+            int CountSpaces(string text)
+            {
+                int count = 0;
+
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (text[i] == ' ')
+                    {
+                        count++;
+                    }
+                }
+
+                return count;
+            }
+        }
+    }
+}
