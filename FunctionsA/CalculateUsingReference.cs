16fc57ef3682113117969858d3644af4db63bd84
Animeology <joseph.nguyen19@yahoo.com>
2023-07-13 15:44:55 -0500

    Create CalculateUsingReference.cs

---
+++ FunctionsA/CalculateUsingReference.cs
@@ -0,0 +1,22 @@
+﻿namespace IntermediateExercises.FunctionsA
+{
+    public class CalculateUsingReference
+    {
+        public static void CalculateRefDouble()
+        {
+            Console.WriteLine("Input a number and it will print the number doubled");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Double(ref num));
+
+            int Double(ref int num)
+            {
+                int result = num * 2;
+
+                return result;
+            }
+
+        }
+    }
+}
