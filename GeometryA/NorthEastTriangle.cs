1edcdd873360d191f8771a06e80b69856a2b9a14
Animeology <joseph.nguyen19@yahoo.com>
2023-07-13 17:46:17 -0500

    Create NorthEastTriangle.cs

---
+++ GeometryA/NorthEastTriangle.cs
@@ -0,0 +1,30 @@
+﻿namespace IntermediateExercises.GeometryA
+{
+    public class NorthEastTriangle
+    {
+        public static void NETriangle()
+        {
+            Console.WriteLine("Input the width to print a '*' NorthEast triangle");
+
+            int height = Convert.ToInt32(Console.ReadLine()!);
+            int width = 0;
+            int max = height;
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Console.Write(' ');
+                }
+
+                for (int k = 0; k < max; k++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+                width++;
+                max--;
+            }
+        }
+    }
+}
