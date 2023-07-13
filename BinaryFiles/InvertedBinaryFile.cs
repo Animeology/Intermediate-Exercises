2b6a1a89453a078b0f7c77c97727220c7d92e73e
Animeology <joseph.nguyen19@yahoo.com>
2023-07-13 13:38:42 -0500

    Create InvertedBinaryFile.cs

---
+++ BinaryFiles/InvertedBinaryFile.cs
@@ -0,0 +1,27 @@
+﻿namespace IntermediateExercises.BinaryFiles
+{
+    public class InvertedBinaryFile
+    {
+        public static void InvertedFile()
+        {
+            string inputFileName = "app.exe";
+            string outputFileName = "app.inv";
+
+            using (FileStream file = File.OpenRead(inputFileName))
+            {
+                long size = file.Length;
+                byte[] data = new byte[size];
+
+                file.Read(data, 0, (int)size);
+
+                using (FileStream outFile = File.OpenWrite(outputFileName))
+                {
+                    for (long i = 0; i < size; i++)
+                    {
+                        outFile.WriteByte(data[i]);
+                    }
+                }
+            }
+        }
+    }
+}
