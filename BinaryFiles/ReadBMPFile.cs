c03edc3f1ce20d90cda7b3f9e685dcb07ff11921
Animeology <joseph.nguyen19@yahoo.com>
2023-07-13 13:38:44 -0500

    Create ReadBMPFile.cs

---
+++ BinaryFiles/ReadBMPFile.cs
@@ -0,0 +1,20 @@
+﻿namespace IntermediateExercises.BinaryFiles
+{
+    public class ReadBMPFile
+    {
+        public static void ReadBMP()
+        {
+            string fileName = "logo.bmp";
+
+            using (FileStream file = File.Open(fileName, FileMode.Open, FileAccess.ReadWrite))
+            {
+                char b1 = (char)file.ReadByte();
+                char b2 = (char)file.ReadByte();
+
+                file.Seek(0, SeekOrigin.Begin);
+                file.WriteByte(Convert.ToByte('B'));
+                file.WriteByte(Convert.ToByte('M'));
+            }
+        }
+    }
+}
