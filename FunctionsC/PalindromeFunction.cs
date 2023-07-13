2ce23e133d6a85e81d69b2c8ddad081ed3511018
Animeology <joseph.nguyen19@yahoo.com>
2023-07-13 17:40:52 -0500

    Create PalindromeFunction.cs

---
+++ FunctionsC/PalindromeFunction.cs
@@ -0,0 +1,31 @@
+﻿namespace IntermediateExercises.FunctionsC
+{
+    public class PalindromeFunction
+    {
+        public static void Palindrome()
+        {
+            Console.WriteLine("Input a string and it will check if it is a palindrome");
+
+            string text = Console.ReadLine()!;
+            string reverse = string.Empty;
+
+            Console.WriteLine(IsPalindrome(text));
+
+            bool IsPalindrome(string text)
+            {
+                for (int i = text.Length - 1; i >= 0; i--)
+                {
+                    reverse += text[i];
+                }
+                if (text == reverse)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
