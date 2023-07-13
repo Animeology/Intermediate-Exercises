1e47a61ca89092ff7baeff4298a6b84571b1a617
Animeology <joseph.nguyen19@yahoo.com>
2023-07-13 17:32:06 -0500

    Create ModifyCharacters.cs

---
+++ FunctionsB/ModifyCharacters.cs
@@ -0,0 +1,24 @@
+﻿namespace IntermediateExercises.FunctionsB
+{
+    public class ModifyCharacters
+    {
+        public static void ModifyCharacter()
+        {
+            Console.WriteLine("Input a text, position of the lettter you want to change and input a new character");
+
+            string text = Console.ReadLine()!;
+            int position = Convert.ToInt32(Console.ReadLine());
+            char letter = Convert.ToChar(Console.Read());
+
+            Console.WriteLine(ChangeLetter(text, position, letter));
+
+            string ChangeLetter(string text, int position, char letter)
+            {
+                text = text.Remove(position, 1);
+                text = text.Insert(position, letter.ToString());
+
+                return text;
+            }
+        }
+    }
+}
