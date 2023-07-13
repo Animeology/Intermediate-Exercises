47a7dc513d227a4df49b4232dc7e359f5e114f3c
Animeology <joseph.nguyen19@yahoo.com>
2023-07-13 13:53:04 -0500

    Create Multiples.cs

---
+++ FlowControlsB/Multiples.cs
@@ -0,0 +1,21 @@
+﻿namespace IntermediateExercises.FlowControlsB
+{
+    public class Multiples
+    {
+        public static void MultipleThreesAndFives()
+        {
+            Console.WriteLine("Prints out numbers ranged from 1 to 500 that are multiples of 3 and 5");
+
+            int x = 1;
+            int y = 500;
+
+            for (int i = x; i <= y; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.Write("{0} ", i);
+                }
+            }
+        }
+    }
+}
