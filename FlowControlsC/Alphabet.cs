08d2bf72fe6e30db085a84ed73fc4a37c75c03c3
Animeology <joseph.nguyen19@yahoo.com>
2023-07-13 14:00:50 -0500

    Create Alphabet.cs

---
+++ FlowControlsC/Alphabet.cs
@@ -0,0 +1,15 @@
+﻿namespace IntermediateExercises.FlowControlsC
+{
+    public class Alphabet
+    {
+        public static void DisplayAlphabet()
+        {
+            Console.WriteLine("Displays the Alphabet");
+
+            for (char c = 'A'; c < 'Z'; c++)
+            {
+                Console.Write("{0} ", c);
+            }
+        }
+    }
+}
