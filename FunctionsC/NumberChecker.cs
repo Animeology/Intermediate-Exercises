3b07240f131629218a332d537438c79e13d36b13
Animeology <joseph.nguyen19@yahoo.com>
2023-07-13 17:40:41 -0500

    Create NumberChecker.cs

---
+++ FunctionsC/NumberChecker.cs
@@ -0,0 +1,26 @@
+﻿namespace IntermediateExercises.FunctionsC
+{
+    public class NumberChecker
+    {
+        public static void CheckNumber()
+        {
+            Console.WriteLine("Input a string to check if the whole string is only alphabetic");
+
+            string text = Console.ReadLine()!;
+
+            Console.WriteLine(IsNumber(text));
+
+            bool IsNumber(string text)
+            {
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (text[i] < '1' || text[i] > '9')
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+    }
+}
