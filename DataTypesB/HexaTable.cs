af552ae90996f65c3af018c7bcd1801e9f73afb1
Animeology <joseph.nguyen19@yahoo.com>
2023-07-13 13:45:43 -0500

    Create HexaTable.cs

---
+++ DataTypesB/HexaTable.cs
@@ -0,0 +1,27 @@
+﻿namespace IntermediateExercises.DataTypesB
+{
+    public class HexaTable
+    {
+        public static void HexadecimalTable()
+        {
+            Console.WriteLine("Prints a hexadecimal table");
+
+            int max = 255;
+
+            for (int i = 0; i <= max; i++)
+            {
+                if (i < 16)
+                {
+                    Console.Write("0");
+                }
+
+                Console.Write("{0} ", Convert.ToString(i, 16));
+
+                if (i % 16 == 15)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
