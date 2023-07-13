b82dbdb705af92bc50180f5e412731a08c8e798e
Animeology <joseph.nguyen19@yahoo.com>
2023-07-13 17:50:01 -0500

    Create ParallelogramShape.cs

---
+++ GeometryB/ParallelogramShape.cs
@@ -0,0 +1,28 @@
+﻿namespace IntermediateExercises.GeometryB
+{
+    public class ParallelogramShape
+    {
+        public static void Parallelogram()
+        {
+            Console.WriteLine("Input the width, height, and symbol to display a parallelogram");
+            int width = Convert.ToInt32(Console.ReadLine());
+            int height = Convert.ToInt32(Console.ReadLine());
+            char symbol = Convert.ToChar(Console.ReadLine()!);
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Console.Write(symbol);
+                }
+
+                Console.WriteLine();
+
+                for (int k = 0; k <= i; k++)
+                {
+                    Console.Write(" ");
+                }
+            }
+        }
+    }
+}
