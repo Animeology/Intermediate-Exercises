187a5783fbf21545bf7d19450b4dd45f89d99b1b
Animeology <joseph.nguyen19@yahoo.com>
2023-07-13 13:38:41 -0500

    Create BMPImageFile.cs

---
+++ BinaryFiles/BMPImageFile.cs
@@ -0,0 +1,31 @@
+﻿namespace IntermediateExercises.BinaryFiles
+{
+    public class BMPImageFile
+    {
+        public static void BMPImage()
+        {
+            string fileName = "logo.bmp";
+            int size = 54;
+            int width, height;
+            byte[] data = new byte[size];
+
+            using (FileStream file = File.OpenRead(fileName))
+            {
+                file.Read(data, 0, size);
+            }
+
+            width = data[18]
+                    + data[19] * 256
+                    + data[20] * 256 * 256
+                    + data[21] * 256 * 256 * 256;
+
+            height = data[22]
+                    + data[23] * 256
+                    + data[24] * 256 * 256
+                    + data[25] * 256 * 256 * 256;
+
+
+            Console.WriteLine("{0}x{1}", width, height);
+        }
+    }
+}
