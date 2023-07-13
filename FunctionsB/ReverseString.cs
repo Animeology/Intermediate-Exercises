ef8acae5a4cb33f7d33fb9a87315ca9b669a232e
Animeology <joseph.nguyen19@yahoo.com>
2023-07-13 17:32:12 -0500

    Create ReverseString.cs

---
+++ FunctionsB/ReverseString.cs
@@ -0,0 +1,21 @@
+﻿namespace IntermediateExercises.FunctionsB
+{
+    public class ReverseString
+    {
+        public static void StringReverse()
+        {
+            Console.WriteLine("Input a text and it will be reverse");
+
+            string text = Console.ReadLine()!;
+
+            InvertString(text);
+
+            void InvertString(string text)
+            {
+                char[] arr = text.ToCharArray();
+                Array.Reverse(arr);
+                Console.WriteLine(arr);
+            }
+        }
+    }
+}
