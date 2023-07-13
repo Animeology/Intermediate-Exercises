eae59cf1a401c0e5d8313f7caa0a5afb54055b71
Animeology <joseph.nguyen19@yahoo.com>
2023-07-13 17:49:59 -0500

    Create HollowSquareShape.cs

---
+++ GeometryB/HollowSquareShape.cs
@@ -0,0 +1,39 @@
+﻿namespace IntermediateExercises.GeometryB
+{
+    public class HollowSquareShape
+    {
+        public static void HollowSquare()
+        {
+            Console.WriteLine("Input a symbol and the width/height to display a hollow square");
+
+            char symbol = Convert.ToChar(Console.ReadLine()!);
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(symbol);
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < size - 2; i++)
+            {
+                Console.Write(symbol);
+
+                for (int j = 0; j < size - 2; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                Console.WriteLine(symbol);
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(symbol);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
