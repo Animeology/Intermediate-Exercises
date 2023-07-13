d07d5fb4d126cbe6a2064a2d295d4a065b69f693
Animeology <joseph.nguyen19@yahoo.com>
2023-07-13 17:46:15 -0500

    Create TriangleShape.cs

---
+++ GeometryA/TriangleShape.cs
@@ -0,0 +1,24 @@
+﻿namespace IntermediateExercises.GeometryA
+{
+    public class TriangleShape
+    {
+        public static void Triangle()
+        {
+            Console.WriteLine("Input the symbol and a width to print a triangle");
+
+            char symbol = Convert.ToChar(Console.ReadLine()!);
+            int width = Convert.ToInt32(Console.ReadLine()!);
+            int height = width;
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Console.Write(symbol);
+                }
+                Console.WriteLine();
+                width--;
+            }
+        }
+    }
+}
