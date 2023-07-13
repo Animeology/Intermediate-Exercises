0c6029cf218327c5865e137dcdb151fdfc4bd9f0
Animeology <joseph.nguyen19@yahoo.com>
2023-07-13 13:45:47 -0500

    Create PrimeFactor.cs

---
+++ DataTypesB/PrimeFactor.cs
@@ -0,0 +1,24 @@
+﻿namespace IntermediateExercises.DataTypesB
+{
+    public class PrimeFactor
+    {
+        public static void PrimeFactors()
+        {
+            Console.WriteLine("Input a integer and it will display the product of its prime factor");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+            int divisor = 2;
+
+            while (num > 1)
+            {
+                while (num % divisor == 0)
+                {
+                    Console.Write(divisor + "x");
+                    num = num / divisor;
+                }
+                divisor++;
+            }
+            Console.WriteLine(1);
+        }
+    }
+}
