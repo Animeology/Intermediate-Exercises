130c6662471291ecc258dd0ff1a028a99804587b
Animeology <joseph.nguyen19@yahoo.com>
2023-07-13 17:40:39 -0500

    Create AlphabetChecker.cs

---
+++ FunctionsC/AlphabetChecker.cs
@@ -0,0 +1,29 @@
+﻿namespace IntermediateExercises.FunctionsC
+{
+    public class AlphabetChecker
+    {
+        public static void CheckAlphabet()
+        {
+            Console.WriteLine("Input a string to check if the whole string is only alphabetic");
+
+            string text = Console.ReadLine()!;
+
+            Console.WriteLine(IsAlphabetic(text));
+
+            bool IsAlphabetic(string text)
+            {
+                text = text.ToUpper();
+
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (text[i] < 'A' || text[i] > 'Z')
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+    }
+}
