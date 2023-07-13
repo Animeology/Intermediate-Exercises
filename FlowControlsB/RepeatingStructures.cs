27c639bd11a7838df4c60ce658894026355d12b3
Animeology <joseph.nguyen19@yahoo.com>
2023-07-13 13:52:54 -0500

    Create RepeatingStructures.cs

---
+++ FlowControlsB/RepeatingStructures.cs
@@ -0,0 +1,39 @@
+﻿namespace IntermediateExercises.FlowControlsB
+{
+    public class RepeatingStructures
+    {
+        public static void RepetitiveStructures()
+        {
+            Console.WriteLine("Input two numbers and it will show the range between these two numbers included the two inputs");
+
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            // For Loop
+            for (int i = x; i <= y; i++)
+            {
+                Console.Write("{0} ", i);
+            }
+
+            Console.WriteLine();
+
+            // While loop
+            int loop = x;
+            while (loop <= y)
+            {
+                Console.Write("{0} ", loop);
+                loop++;
+            }
+
+            Console.WriteLine();
+
+            // Do While loop
+            int j = x;
+            do
+            {
+                Console.Write("{0} ", j);
+                j++;
+            } while (j <= y);
+        }
+    }
+}
