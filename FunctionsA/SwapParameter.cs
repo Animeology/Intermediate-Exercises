7f01549977b2bd134c7ffab2e648550a5c97bff6
Animeology <joseph.nguyen19@yahoo.com>
2023-07-13 15:44:53 -0500

    Create SwapParameter.cs

---
+++ FunctionsA/SwapParameter.cs
@@ -0,0 +1,24 @@
+﻿namespace IntermediateExercises.FunctionsA
+{
+    public class SwapParameter
+    {
+        public static void SwapParameters()
+        {
+            Console.WriteLine("Input two numbers and it will swap places.");
+
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Swap(ref x, ref y);
+
+            void Swap(ref int x, ref int y)
+            {
+                int temp;
+                temp = x;
+                x = y;
+                y = temp;
+                Console.WriteLine("{0} {1}", x, y);
+            }
+        }
+    }
+}
