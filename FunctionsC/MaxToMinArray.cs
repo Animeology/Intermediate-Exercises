ca4d2058e87ca84bc4280729d85f3faf1077335e
Animeology <joseph.nguyen19@yahoo.com>
2023-07-13 17:40:51 -0500

    Create MaxToMinArray.cs

---
+++ FunctionsC/MaxToMinArray.cs
@@ -0,0 +1,40 @@
+﻿namespace IntermediateExercises.FunctionsC
+{
+    public class MaxToMinArray
+    {
+        public static void MaxMinArray()
+        {
+            Console.WriteLine("Input 5 numbers into the array and it will find the max and min number in the array");
+
+            int count = 5;
+            int[] arr = new int[count];
+
+            int max = int.MinValue;
+            int min = int.MaxValue;
+
+            for (int i = 0; i < count; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            FindMinMax(arr, max, min);
+
+            void FindMinMax(int[] arr, int max, int min)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] > max)
+                    {
+                        max = arr[i];
+                    }
+                    if (arr[i] < min)
+                    {
+                        min = arr[i];
+                    }
+                }
+                Console.WriteLine("Min: {0}", min);
+                Console.WriteLine("Max: {0}", max);
+            }
+        }
+    }
+}
