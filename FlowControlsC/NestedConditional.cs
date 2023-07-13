7fa2a910aa9ec58ada7ff93b29f8464d87eea9fa
Animeology <joseph.nguyen19@yahoo.com>
2023-07-13 14:00:53 -0500

    Create NestedConditional.cs

---
+++ FlowControlsC/NestedConditional.cs
@@ -0,0 +1,17 @@
+﻿namespace IntermediateExercises.FlowControlsC
+{
+    public class NestedConditional
+    {
+        public static void NestedConditionalOp()
+        {
+            Console.WriteLine("Input two numbers and it will check how many of them are positive");
+
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            var pos = (a > 0 && b > 0) ? 2 : ((a > 0 || b > 0) ? 1 : 0);
+
+            Console.WriteLine(pos);
+        }
+    }
+}
