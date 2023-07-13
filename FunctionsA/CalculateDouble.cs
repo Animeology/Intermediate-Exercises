771546a47944b393fe3ca895d4dea641cdc93eab
Animeology <joseph.nguyen19@yahoo.com>
2023-07-13 15:44:50 -0500

    Create CalculateDouble.cs

---
+++ FunctionsA/CalculateDouble.cs
@@ -0,0 +1,21 @@
+﻿namespace IntermediateExercises.FunctionsA
+{
+    public class CalculateDouble
+    {
+        public static void CalculateDoubles()
+        {
+            Console.WriteLine("Input a number and it will print the number doubled");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Double(num));
+
+            int Double(int num)
+            {
+                int result = num * 2;
+
+                return result;
+            }
+        }
+    }
+}
