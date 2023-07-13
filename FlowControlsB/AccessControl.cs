9c28c48cc1faa4e553ca4cdaff4c2eee2f16a7e7
Animeology <joseph.nguyen19@yahoo.com>
2023-07-13 13:53:02 -0500

    Create AccessControl.cs

---
+++ FlowControlsB/AccessControl.cs
@@ -0,0 +1,36 @@
+﻿namespace IntermediateExercises.FlowControlsB
+{
+    public class AccessControl
+    {
+        public static void Login()
+        {
+            Console.WriteLine("Try to login with the username and password");
+
+            int attempts = 0;
+
+            while (attempts < 3)
+            {
+                Console.Write("Username: ");
+                string username = Console.ReadLine()!;
+
+                Console.Write("Password: ");
+                string password = Console.ReadLine()!;
+
+                if (username == "12" && password == "1234")
+                {
+                    Console.WriteLine("Login Successful!");
+                }
+                else
+                {
+                    Console.WriteLine("Login Failed!");
+                }
+                attempts++;
+
+                if (attempts == 3)
+                {
+                    Console.WriteLine("You used all your attempts!");
+                }
+            }
+        }
+    }
+}
