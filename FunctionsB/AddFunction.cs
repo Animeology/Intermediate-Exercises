446e4c5f0e91e60d42668b16e07ef2604ff432b7
Animeology <joseph.nguyen19@yahoo.com>
2023-07-13 17:32:10 -0500

    Create AddFunction.cs

---
+++ FunctionsB/AddFunction.cs
@@ -0,0 +1,26 @@
+﻿namespace IntermediateExercises.FunctionsB
+{
+    public class AddFunction
+    {
+        public static void Add()
+        {
+            Console.WriteLine("Input a number and it will add to the current sum");
+
+            string text = Console.ReadLine()!;
+
+            Console.WriteLine(AddDigits(text));
+
+            int AddDigits(string text)
+            {
+                int sum = 0;
+
+                for (int i = 0; i < text.Length; i++)
+                {
+                    sum += Convert.ToInt32(text.Substring(i, 1));
+                }
+
+                return sum;
+            }
+        }
+    }
+}
