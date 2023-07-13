601f1eac127eca7a6bb3bf50dbf8f5ecd1d1e20e
Animeology <joseph.nguyen19@yahoo.com>
2023-07-13 17:32:18 -0500

    Create TitleFormat.cs

---
+++ FunctionsB/TitleFormat.cs
@@ -0,0 +1,49 @@
+﻿namespace IntermediateExercises.FunctionsB
+{
+    public class TitleFormat
+    {
+        public static void Title()
+        {
+            Console.WriteLine("Input a string and it will print it like a title");
+
+            string text = Console.ReadLine()!;
+
+            WriteTitle(text);
+
+            void WriteTitle(string text)
+            {
+                text = text.ToUpper();
+
+                for (int i = 0; i < text.Length * 2 - 1; i++)
+                {
+                    Console.Write('-');
+                }
+                Console.WriteLine();
+
+                for (int i = 0; i < text.Length * 2 - 1; i++)
+                {
+                    Console.Write(' ');
+                }
+                Console.WriteLine();
+
+                for (int i = 0; i < text.Length; i++)
+                {
+                    Console.Write(text[i] + " ");
+                }
+                Console.WriteLine();
+
+                for (int i = 0; i < text.Length * 2 - 1; i++)
+                {
+                    Console.Write(' ');
+                }
+                Console.WriteLine();
+
+                for (int i = 0; i < text.Length * 2 - 1; i++)
+                {
+                    Console.Write('-');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
