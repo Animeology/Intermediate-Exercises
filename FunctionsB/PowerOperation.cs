1dd9dce5d6e38ffb21719724882ba631b487d5b1
Animeology <joseph.nguyen19@yahoo.com>
2023-07-13 17:32:16 -0500

    Create PowerOperation.cs

---
+++ FunctionsB/PowerOperation.cs
@@ -0,0 +1,22 @@
+﻿namespace IntermediateExercises.FunctionsB
+{
+    public class PowerOperation
+    {
+        public static void PowerOp()
+        {
+            Console.WriteLine("Input 2 numbers and the it will calculate the first number of the power of the second number");
+
+            double num = Convert.ToInt32(Console.ReadLine());
+            double power = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Power(num, power));
+
+            double Power(double num, double power)
+            {
+                double result = Math.Pow(num, power);
+
+                return result;
+            }
+        }
+    }
+}
