4dd1d665a220b861076d7495f7ac6fc6584a6da8
Animeology <joseph.nguyen19@yahoo.com>
2023-07-14 14:40:38 -0500

    Create PhotoBook.cs

---
+++ Structures/PhotoBook.cs
@@ -0,0 +1,32 @@
+﻿namespace IntermediateExercises.Structures
+{
+    public class PhotoBook
+    {
+        protected int numPages;
+
+        public PhotoBook()
+        {
+            numPages = 16;
+        }
+
+        public PhotoBook(int pages)
+        {
+            numPages = pages;
+            // this.numPages = pages also works
+        }
+
+        public int GetNumberPages()
+        {
+            return numPages;
+        }
+    }
+
+    public class BigPhotoBook : PhotoBook
+    {
+        public BigPhotoBook()
+        {
+            numPages = 64;
+        }
+    }
+}
+
