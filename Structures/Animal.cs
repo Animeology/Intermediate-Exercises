2bdde541bedc9cf3b407ada327c5d598794e15c5
Animeology <joseph.nguyen19@yahoo.com>
2023-07-14 14:35:12 -0500

    Create Animal.cs

---
+++ Structures/Animal.cs
@@ -0,0 +1,18 @@
+﻿namespace IntermediateExercises.Structures
+{
+    public abstract class Animal
+    {
+        string m_name = string.Empty;
+
+        public void SetName(string name)
+        {
+            m_name = name;
+        }
+        public string GetName()
+        {
+            return m_name;
+        }
+        public abstract void Eat();
+
+    }
+}
