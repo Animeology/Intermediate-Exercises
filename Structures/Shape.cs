65e133b1772f508f5b55a13ef61a3846e1a43515
Animeology <joseph.nguyen19@yahoo.com>
2023-07-14 14:39:18 -0500

    Create Shape.cs

---
+++ Structures/Shape.cs
@@ -0,0 +1,34 @@
+﻿namespace IntermediateExercises.Structures
+{
+    public class Shape : Location
+    {
+        protected Location? location;
+
+        public override string ToString()
+        {
+            return string.Empty;
+        }
+
+        public double Area()
+        {
+            return 0.0;
+        }
+
+        public double Perimeter()
+        {
+            return 0.0;
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        protected double side1;
+        protected double side2;
+    }
+
+    public class Circle : Shape
+    {
+        protected double radius;
+    }
+
+}
