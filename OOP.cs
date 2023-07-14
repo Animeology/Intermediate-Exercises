770360608a0bcbe2cacbe1d8b667d3f45661b226
Animeology <joseph.nguyen19@yahoo.com>
2023-07-14 17:39:00 -0500

    Update OOP.cs

--- OOP.cs
+++ OOP.cs
@@ -1,4 +1,6 @@
-﻿namespace Intermediate_Exercises
+﻿using IntermediateExercises.Structures;
+
+namespace Intermediate_Exercises
 {
     public class OOP
     {
@@ -6,33 +8,12 @@
         {
             Dog dog = new Dog();
             string name = Console.ReadLine()!;
+
             dog.SetName(name);
+
             Console.WriteLine(dog.GetName());
             dog.Eat();
         }
-        public class Dog : Animal
-        {
-            public override void Eat()
-            {
-                Console.WriteLine("Eating");
-            }
-        }
-
-        public abstract class Animal
-        {
-            string m_name = string.Empty;
-
-            public void SetName(string name)
-            {
-                m_name = name;
-            }
-            public string GetName()
-            {
-                return m_name;
-            }
-            public abstract void Eat();
-        }
-
 
         public void CarDriving()
         {
@@ -46,77 +27,6 @@
             {
                 car.Drive();
             }
-        }
-
-        public class Car : IVehicle
-        {
-            public int Gasoline { get; set; }
-
-            public Car(int gasoline)
-            {
-                Gasoline = gasoline;
-            }
-
-            public void Drive()
-            {
-                if (Gasoline > 0)
-                {
-                    Console.WriteLine("Driving");
-                }
-                else
-                {
-                    Console.WriteLine("Ran out of gasoline");
-                }
-            }
-
-            public bool Refuel(int gasoline)
-            {
-                Gasoline += gasoline;
-                return true;
-            }
-        }
-
-        public interface IVehicle
-        {
-            void Drive();
-            bool Refuel(int gasoline);
-        }
-
-        public class Location
-        {
-            double x;
-            double y;
-        }
-
-        public class Shape : Location
-        {
-            protected Location? location;
-
-            public override string ToString()
-            {
-                return string.Empty;
-            }
-
-            public double Area()
-            {
-                return 0.0;
-            }
-
-            public double Perimeter()
-            {
-                return 0.0;
-            }
-        }
-
-        public class Rectangle : Shape
-        {
-            protected double side1;
-            protected double side2;
-        }
-
-        public class Circle : Shape
-        {
-            protected double radius;
         }
 
         public void InheritanceObjects()
@@ -165,35 +75,6 @@
             Console.WriteLine(album3.GetNumberPages());
         }
 
-        public class PhotoBook
-        {
-            protected int numPages;
-
-            public PhotoBook()
-            {
-                numPages = 16;
-            }
-
-            public PhotoBook(int pages)
-            {
-                numPages = pages;
-                // this.numPages = pages also works
-            }
-
-            public int GetNumberPages()
-            {
-                return numPages;
-            }
-        }
-
-        public class BigPhotoBook : PhotoBook
-        {
-            public BigPhotoBook()
-            {
-                numPages = 64;
-            }
-        }
-
         public void StudentTeacherTest()
         {
             Person person = new Person("Joseph");
@@ -211,67 +92,6 @@
             teacher.Explain();
         }
 
-        public class Person
-        {
-            public string m_name { get; set; }
-            public int m_age { get; set; }
-
-            public Person(string name)
-            {
-                m_name = name;
-            }
-
-            public override string ToString()
-            {
-                return "Hello!, My name is " + m_name;
-            }
-
-            ~Person()
-            {
-                m_name = string.Empty;
-            }
-
-            public void Greet()
-            {
-                Console.WriteLine("Hello!");
-            }
-
-            public void SetAge(int age)
-            {
-                m_age = age;
-            }
-        }
-
-        public class Student : Person
-        {
-            public Student(string name) : base(name)
-            {
-                m_name = name;
-            }
-
-            public void Study()
-            {
-                Console.WriteLine("I'm studying");
-            }
-
-            public void ShowAge()
-            {
-                Console.WriteLine("My age is: {0} years old", m_age);
-            }
-        }
-
-        public class Teacher : Person
-        {
-            public Teacher(string name) : base(name)
-            {
-                m_name = name;
-            }
-
-            public void Explain()
-            {
-                Console.WriteLine("I'm explaining");
-            }
-        }
 
         public void PersonNameConAndDes()
         {
