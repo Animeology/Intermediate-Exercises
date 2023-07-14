110fe458c9b849df4e62bf1a74ce7f0a79633220
Animeology <joseph.nguyen19@yahoo.com>
2023-07-14 17:38:58 -0500

    Create Person.cs

---
+++ Structures/Person.cs
@@ -0,0 +1,63 @@
+﻿namespace IntermediateExercises.Structures
+{
+    public class Person
+    {
+        public string m_name { get; set; }
+        public int m_age { get; set; }
+
+        public Person(string name)
+        {
+            m_name = name;
+        }
+
+        public override string ToString()
+        {
+            return "Hello!, My name is " + m_name;
+        }
+
+        ~Person()
+        {
+            m_name = string.Empty;
+        }
+
+        public void Greet()
+        {
+            Console.WriteLine("Hello!");
+        }
+
+        public void SetAge(int age)
+        {
+            m_age = age;
+        }
+    }
+    public class Student : Person
+    {
+        public Student(string name) : base(name)
+        {
+            m_name = name;
+        }
+
+        public void Study()
+        {
+            Console.WriteLine("I'm studying");
+        }
+
+        public void ShowAge()
+        {
+            Console.WriteLine("My age is: {0} years old", m_age);
+        }
+    }
+
+    public class Teacher : Person
+    {
+        public Teacher(string name) : base(name)
+        {
+            m_name = name;
+        }
+
+        public void Explain()
+        {
+            Console.WriteLine("I'm explaining");
+        }
+    }
+}
