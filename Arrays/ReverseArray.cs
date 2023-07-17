d44738ff8fcd3608d8f90e60dca25aebed175e4e
Animeology <joseph.nguyen19@yahoo.com>
2023-07-17 13:26:32 -0500

    Update ReverseArray.cs

--- Arrays/ReverseArray.cs
+++ Arrays/ReverseArray.cs
@@ -1,24 +1,26 @@
 ﻿namespace IntermediateExercises.Arrays
 {
+    using Base;
+
     public class ReverseArray
     {
         public static void ReversingTheArray()
         {
-            Console.WriteLine("Input 5 numbers and it will print it in reverse array");
+            Printing.PrintLine("Input 5 numbers and it will print it in reverse array");
 
             int max = 5;
             int[] nums = new int[max];
 
             for (int i = 0; i < max; i++)
             {
-                nums[i] = Convert.ToInt32(Console.ReadLine());
+                nums[i] = InputChecker.InputInt();
             }
 
             Array.Reverse(nums);
 
             for (int i = 0; i < max; i++)
             {
-                Console.Write("{0} ", nums[i]);
+                Printing.Print($"{nums[i]} ");
             }
         }
     }
