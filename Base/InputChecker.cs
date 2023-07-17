fa610fc31ded6e5cb758e0ce0eeac21a628371d6
Animeology <joseph.nguyen19@yahoo.com>
2023-07-17 13:09:11 -0500

    Create InputChecker.cs

---
+++ Base/InputChecker.cs
@@ -0,0 +1,10 @@
+﻿namespace IntermediateExercises.Base
+{
+    public class InputChecker
+    {
+        public static int InputInt()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
