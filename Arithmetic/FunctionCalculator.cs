2f3fe4401f98857e6e70a22a0afc7a4f7dd9667d
Animeology <joseph.nguyen19@yahoo.com>
2023-07-17 13:09:17 -0500

    Update FunctionCalculator.cs

--- Arithmetic/FunctionCalculator.cs
+++ Arithmetic/FunctionCalculator.cs
@@ -1,19 +1,21 @@
 ﻿namespace IntermediateExercises.Arithmetic
 {
+    using Base;
+
     public class FunctionCalculator
     {
         public static void CalculateFunction()
         {
-            Console.WriteLine("Input two numbers for a range to calculate the following: y = x^2 - 2x + 1");
+            Printing.PrintLine("Input two numbers for a range to calculate the following: y = x^2 - 2x + 1");
 
-            int x = Convert.ToInt32(Console.ReadLine());
-            int y = Convert.ToInt32(Console.ReadLine());
+            int x = InputChecker.InputInt();
+            int y = InputChecker.InputInt();
 
             for (int i = x; i <= y; i++)
             {
                 double function = Math.Pow(i, 2) - 2 * i + 1;
 
-                Console.Write("{0} ", function);
+                Printing.Print($"{function} ");
             }
         }
     }
