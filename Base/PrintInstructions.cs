d7dd3a067b3b0d8dd4b1c4e0df3d0a7ee3082aaf
Animeology <joseph.nguyen19@yahoo.com>
2023-07-17 13:02:16 -0500

    Create PrintInstructions.cs

---
+++ Base/PrintInstructions.cs
@@ -0,0 +1,10 @@
+﻿namespace IntermediateExercises.Base
+{
+    public class PrintInstructions
+    {
+        public static void PrintLine(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
