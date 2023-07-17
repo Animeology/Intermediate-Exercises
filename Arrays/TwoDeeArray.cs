b6a469f66beed84e825f14d40da33141d35beefd
Animeology <joseph.nguyen19@yahoo.com>
2023-07-17 13:36:02 -0500

    Update TwoDeeArray.cs

--- Arrays/TwoDeeArray.cs
+++ Arrays/TwoDeeArray.cs
@@ -1,10 +1,12 @@
 ﻿namespace IntermediateExercises.Arrays
 {
+    using Base;
+
     public class TwoDeeArray
     {
         public static void TwoDeeArrays()
         {
-            Console.WriteLine("Input 10 grades and it will split it into 2 groups of 5 and calculate the average of each group");
+            Printing.PrintLine("Input 10 grades and it will split it into 2 groups of 5 and calculate the average of each group");
 
             int groupBy = 5;
             int[,] allGrades = new int[groupBy, groupBy];
@@ -13,23 +15,25 @@
             {
                 for (int j = 0; j < groupBy; j++)
                 {
-                    allGrades[i, j] = Convert.ToInt32(Console.ReadLine());
+                    allGrades[i, j] = InputChecker.InputInt();
                 }
             }
 
-            int group1Total = 0;
+            int groupOneTotal = 0;
             for (int i = 0; i < groupBy; i++)
             {
-                group1Total += allGrades[0, i];
+                groupOneTotal += allGrades[0, i];
             }
-            Console.WriteLine("Average for group 1 is {0}", group1Total / groupBy);
+            float groupOneAverage = groupOneTotal / groupBy;
+            Printing.PrintLine($"Average for group 1 is {groupOneAverage}");
 
-            int group2Total = 0;
+            int groupTwoTotal = 0;
             for (int i = 0; i < groupBy; i++)
             {
-                group2Total += allGrades[1, i];
+                groupTwoTotal += allGrades[1, i];
             }
-            Console.WriteLine("Average for group 2 is {0}", group2Total / groupBy);
+            float groupTwoAverage = groupTwoTotal / groupBy;
+            Printing.PrintLine($"Average for group 2 is {groupOneAverage}");
         }
     }
 }
