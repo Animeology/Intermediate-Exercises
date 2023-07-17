0a04418f2f80c32ff7b7f824bb1d392054666e5e
Animeology <joseph.nguyen19@yahoo.com>
2023-07-17 13:38:47 -0500

    Update TwoDeeCharArray.cs

--- Arrays/TwoDeeCharArray.cs
+++ Arrays/TwoDeeCharArray.cs
@@ -1,10 +1,12 @@
 ﻿namespace IntermediateExercises.Arrays
 {
+    using Base;
+
     public class TwoDeeCharArray
     {
         public static void DrawTwoDeeCharArray()
         {
-            Console.WriteLine("Draws a 20(row) by 70(col) of 'X'");
+            Printing.PrintLine("Draws a 20(row) by 70(col) of 'X'");
 
             int rows = 20;
             int cols = 70;
@@ -17,9 +19,9 @@
                 for (int j = 0; j < cols; j++)
                 {
                     arr[i, j] = x;
-                    Console.Write(arr[i, j]);
+                    Printing.PrintInt(arr[i, j]);
                 }
-                Console.WriteLine();
+                Printing.NewLine();
             }
         }
     }
