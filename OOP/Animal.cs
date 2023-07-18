c26fe37f4fb751da9c52161984f7689580b02a81
Animeology <joseph.nguyen19@yahoo.com>
2023-07-17 22:21:43 -0500

    Create Animal.cs

---
+++ OOP/Animal.cs
@@ -0,0 +1,18 @@
+﻿namespace IntermediateExercises.OOP
+{
+    using Structures;
+
+    public class Animal
+    {
+        public static void AnimalEating()
+        {
+            Dog dog = new Dog();
+            string name = Console.ReadLine()!;
+
+            dog.SetName(name);
+
+            Console.WriteLine(dog.GetName());
+            dog.Eat();
+        }
+    }
+}
