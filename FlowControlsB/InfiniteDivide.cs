467e397fc3efbd5e9f3043d517201574ca9f8e9f
Animeology <joseph.nguyen19@yahoo.com>
2023-07-17 20:03:19 -0500

    Update InfiniteDivide.cs

--- FlowControlsB/InfiniteDivide.cs
+++ FlowControlsB/InfiniteDivide.cs
@@ -1,35 +1,38 @@
 ﻿namespace IntermediateExercises.FlowControlsB
 {
+    using Base;
+
     public class InfiniteDivide
     {
         public static void KeepOnDividing()
         {
-            Console.WriteLine("Input two numbers and it would keep dividing until the first input is 0");
+            Printing.PrintLine("Input two numbers and it would keep dividing until the first input is 0");
 
             int num1;
             int num2;
 
             do
             {
-                num1 = Convert.ToInt32(Console.ReadLine());
+                num1 = InputChecker.InputInt();
 
                 if (num1 != 0)
                 {
-                    num2 = Convert.ToInt32(Console.ReadLine());
+                    num2 = InputChecker.InputInt();
                     if (num2 == 0)
                     {
-                        Console.WriteLine("You cannot divide by 0");
+                        Printing.PrintLine("You cannot divide by 0");
                     }
                     else
                     {
                         int result = num1 / num2;
-                        Console.WriteLine("The division is {0}", result);
-                        Console.WriteLine("The rest is {0}", num1 % num2);
+                        int reminding = num1 % num2;
+                        Printing.PrintLine($"The division is {result}");
+                        Printing.PrintLine($"The rest is {reminding}");
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Goodbye!");
+                    Printing.PrintLine("Goodbye!");
                 }
 
             } while (num1 != 0);
