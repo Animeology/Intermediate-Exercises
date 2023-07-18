53699b027a2f60c919c625402587d2d192d70c70
Animeology <joseph.nguyen19@yahoo.com>
2023-07-17 22:21:47 -0500

    Create PersonDisplay.cs

---
+++ OOP/PersonDisplay.cs
@@ -0,0 +1,28 @@
+﻿namespace IntermediateExercises.OOP
+{
+    using Base;
+    using Structures;
+
+    public class PersonDisplay
+    {
+        public void PersonNameConAndDes()
+        {
+            Console.WriteLine("Input 3 names and it will store in the array and displays it");
+
+            int count = 3;
+
+            Person[] persons = new Person[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                string name = InputChecker.InputString();
+                persons[i] = new Person(name);
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(persons[i].ToString());
+            }
+        }
+    }
+}
