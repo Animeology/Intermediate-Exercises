ec91b9fab5c0b2a4ac799efc2553c27ac2e89257
Animeology <joseph.nguyen19@yahoo.com>
2023-07-17 22:21:41 -0500

    Create StudentAndTeacher.cs

---
+++ OOP/StudentAndTeacher.cs
@@ -0,0 +1,24 @@
+﻿namespace IntermediateExercises.OOP
+{
+    using Structures;
+
+    public class StudentAndTeacher
+    {
+        public static void StudentTeacherTest()
+        {
+            Person person = new Person("Joseph");
+            person.Greet();
+
+            Student student = new Student("Joseph");
+            student.SetAge(25);
+            student.Greet();
+            student.ShowAge();
+            student.Study();
+
+            Teacher teacher = new Teacher("Tanner");
+            teacher.SetAge(40);
+            teacher.Greet();
+            teacher.Explain();
+        }
+    }
+}
