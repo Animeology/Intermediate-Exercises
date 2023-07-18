290b4147c2afdfe1a0bfc6fda7946a7cd4f5c9ac
Animeology <joseph.nguyen19@yahoo.com>
2023-07-18 18:07:13 -0500

    Update RightTriangle.cs

--- GeometryA/RightTriangle.cs
+++ GeometryA/RightTriangle.cs
@@ -1,19 +1,21 @@
 ﻿namespace IntermediateExercises.GeometryA
 {
+    using Base;
+
     public class RightTriangle
     {
         public static void RightAlignTriangle()
         {
-            Console.WriteLine("Input a string and it will make a right-aligned triangle");
+            Printing.PrintLine("Input a string and it will make a right-aligned triangle");
 
-            string input = Console.ReadLine()!;
+            string input = InputChecker.InputStringOnlyLetters();
             int height = input.Length - 1;
             int width = 1;
 
             Console.WriteLine();
             for (int i = height; i >= 0; i--)
             {
-                Console.WriteLine(input.Substring(i, width));
+                Printing.PrintLine(input.Substring(i, width));
                 width++;
             }
         }
