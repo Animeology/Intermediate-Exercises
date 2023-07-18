f902aa013b8c1a31e853e7a69e10a4885f73712a
Animeology <joseph.nguyen19@yahoo.com>
2023-07-17 22:21:45 -0500

    Create DrivingCar.cs

---
+++ OOP/DrivingCar.cs
@@ -0,0 +1,21 @@
+﻿namespace IntermediateExercises.OOP
+{
+    using Structures;
+
+    public class DrivingCar
+    {
+        public void CarDriving()
+        {
+            Car car = new Car(0);
+
+            Console.WriteLine("Put how much gasoline you want to put into the car");
+            int gasoline = Convert.ToInt32(Console.ReadLine());
+            // int gasoline = int.Parse(Console.ReadLine()) also works
+
+            if (car.Refuel(gasoline))
+            {
+                car.Drive();
+            }
+        }
+    }
+}
