ebf9bfd86b24f825f2abba82a4a6cbe003e4bee2
Animeology <joseph.nguyen19@yahoo.com>
2023-07-18 14:30:36 -0500

    Update SearchMaxValue.cs

--- FunctionsB/SearchMaxValue.cs
+++ FunctionsB/SearchMaxValue.cs
@@ -1,20 +1,22 @@
 ﻿namespace IntermediateExercises.FunctionsB
 {
+    using Base;
+
     public class SearchMaxValue
     {
         public static void GreatestValue()
         {
-            Console.WriteLine("Request 5 numbers and it will display the highest number");
+            Printing.PrintLine("Request 5 numbers and it will display the highest number");
 
             int count = 5;
             int[] arr = new int[count];
 
             for (int i = 0; i < count; i++)
             {
-                arr[i] = Convert.ToInt32(Console.ReadLine());
+                arr[i] = InputChecker.InputInt();
             }
 
-            Console.WriteLine(SearchGreatestValue(arr));
+            Printing.PrintInt(SearchGreatestValue(arr));
 
             int SearchGreatestValue(int[] arr)
             {
