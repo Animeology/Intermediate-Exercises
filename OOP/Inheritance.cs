633de63cb9455dfad2128e80d4b5cf5a2ba9122b
Animeology <joseph.nguyen19@yahoo.com>
2023-07-17 22:21:46 -0500

    Create Inheritance.cs

---
+++ OOP/Inheritance.cs
@@ -0,0 +1,44 @@
+﻿namespace IntermediateExercises.OOP
+{
+    using Structures;
+    using Base;
+
+    public class Inheritance
+    {
+        public static void InheritanceObjects()
+        {
+            Printing.PrintLine("Input 3 names and it will store in the array and displays it");
+
+            int count = 3;
+
+            Person[] persons = new Person[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                if (i == 0)
+                {
+                    string name = InputChecker.InputString();
+                    persons[i] = new Teacher(name);
+                }
+                else
+                {
+                    string name = InputChecker.InputString();
+                    persons[i] = new Student(name);
+                }
+
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                if (i == 0)
+                {
+                    ((Teacher)persons[i]).Explain();
+                }
+                else
+                {
+                    ((Student)persons[i]).Study();
+                }
+            }
+        }
+    }
+}
