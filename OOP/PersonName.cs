48152b61f75f2951160e5a503530f3233288b3be
Animeology <joseph.nguyen19@yahoo.com>
2023-07-17 22:21:48 -0500

    Create PersonName.cs

---
+++ OOP/PersonName.cs
@@ -0,0 +1,28 @@
+﻿namespace IntermediateExercises.OOP
+{
+    using Base;
+    using Structures;
+
+    public class PersonName
+    {
+        public void PersonNameInArray()
+        {
+            Printing.PrintLine("Input 3 names and it will store in the array and displays it");
+
+            int count = 3;
+
+            Person[] persons = new Person[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                string name = InputChecker.InputString();
+                persons[i] = new Person(name);
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                Printing.PrintLine(persons[i].ToString());
+            }
+        }
+    }
+}
