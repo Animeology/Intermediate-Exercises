9c94c7a5c13387a9db8d6327fd4eb001d1b2e23c
Animeology <joseph.nguyen19@yahoo.com>
2023-07-17 22:21:50 -0500

    Create PhotobookTest.cs

---
+++ OOP/PhotobookTest.cs
@@ -0,0 +1,19 @@
+﻿namespace IntermediateExercises.OOP
+{
+    using Structures;
+
+    public class PhotobookTest
+    {
+        public static void PhotoBookTest()
+        {
+            PhotoBook album1 = new PhotoBook();
+            Console.WriteLine(album1.GetNumberPages());
+
+            PhotoBook album2 = new PhotoBook(24);
+            Console.WriteLine(album2.GetNumberPages());
+
+            BigPhotoBook album3 = new BigPhotoBook();
+            Console.WriteLine(album3.GetNumberPages());
+        }
+    }
+}
