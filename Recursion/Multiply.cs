a915cfe70a445deb381534c53903cad8bcda7241
Animeology <joseph.nguyen19@yahoo.com>
2023-07-19 12:55:38 -0500

    Create Multiply.cs

---
+++ Recursion/Multiply.cs
@@ -0,0 +1,29 @@
+﻿namespace IntermediateExercises.Recursion
+{
+    using Base;
+
+    public class Multiply
+    {
+        public void MultiplyRecursion()
+        {
+            Printing.PrintLine("Input 2 numbers and it will multiply it by recursion");
+
+            int x = InputChecker.InputInt();
+            int y = InputChecker.InputInt();
+
+            Printing.PrintIntLine(Multiply(x, y));
+
+            int Multiply(int x, int y)
+            {
+                if (y == 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return x + Multiply(x, y - 1);
+                }
+            }
+        }
+    }
+}
