acc0bf73f29ec8184726925c8927f154fc4b9639
Animeology <joseph.nguyen19@yahoo.com>
2023-07-19 12:55:41 -0500

    Create Power.cs

---
+++ Recursion/Power.cs
@@ -0,0 +1,29 @@
+﻿namespace IntermediateExercises.Recursion
+{
+    using Base;
+
+    public class Power
+    {
+        public void PowerRecursion()
+        {
+            Printing.PrintLine("Input two numbers and it will do the power of the number using recursion");
+
+            int num = InputChecker.InputInt();
+            int power = InputChecker.InputInt();
+
+            Printing.PrintIntLine(Power(num, power));
+
+            int Power(int num, int power)
+            {
+                if (power == 0)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return num * Power(num, power - 1);
+                }
+            }
+        }
+    }
+}
