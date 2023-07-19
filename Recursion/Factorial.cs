c48728233aee56aca5786aebb31c8912756ba429
Animeology <joseph.nguyen19@yahoo.com>
2023-07-19 12:55:32 -0500

    Create Factorial.cs

---
+++ Recursion/Factorial.cs
@@ -0,0 +1,27 @@
+﻿namespace IntermediateExercises.Recursion
+{
+    using Base;
+
+    public class Factorial
+    {
+        public void FactorialRecursion()
+        {
+            Printing.PrintLine("A Factorial that starts with the user input");
+            int num = InputChecker.InputInt();
+
+            Printing.PrintIntLine(Factorial(num));
+
+            int Factorial(int num)
+            {
+                if (num == 0)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return num * Factorial(num - 1);
+                }
+            }
+        }
+    }
+}
