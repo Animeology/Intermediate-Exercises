25e45648e34b16b954225cde0c43c150cf11e56f
Animeology <joseph.nguyen19@yahoo.com>
2023-07-19 12:55:42 -0500

    Create Reverse.cs

---
+++ Recursion/Reverse.cs
@@ -0,0 +1,29 @@
+﻿namespace IntermediateExercises.Recursion
+{
+    using Base;
+
+    public class Reverse
+    {
+        public void ReverseRecursion()
+        {
+            Printing.PrintLine("Input a string and it will be reverse by recursion");
+            string text = InputChecker.InputStringOnlyLetters();
+
+            Printing.PrintLine(Reverse(text));
+
+            string Reverse(string text)
+            {
+                if (text.Length <= 1)
+                {
+                    return text;
+                }
+                else
+                {
+                    char firstLetter = text[0];
+                    string restOfWord = text.Substring(1);
+                    return Reverse(restOfWord) + firstLetter;
+                }
+            }
+        }
+    }
+}
