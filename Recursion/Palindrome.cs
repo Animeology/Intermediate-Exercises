a9ff975a601ed8b1d967539b7e7a32406f3fbd23
Animeology <joseph.nguyen19@yahoo.com>
2023-07-19 12:55:39 -0500

    Create Palindrome.cs

---
+++ Recursion/Palindrome.cs
@@ -0,0 +1,34 @@
+﻿namespace IntermediateExercises.Recursion
+{
+    using Base;
+
+    public class Palindrome
+    {
+        public void PalindromeRecursion()
+        {
+            Printing.PrintLine("Input a string and it will check if it is a palindrome by recursion");
+            string text = InputChecker.InputStringOnlyLetters();
+
+            Printing.PrintBool(Palindrome(text));
+
+            bool Palindrome(string text)
+            {
+                if (text.Length <= 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    if (text[0] != text[text.Length - 1])
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        return Palindrome(text.Substring(1, text.Length - 2));
+                    }
+                }
+            }
+        }
+    }
+}
