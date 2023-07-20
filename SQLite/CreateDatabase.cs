4d801af7cb633a567501fa82c6ecff3731316793
Animeology <joseph.nguyen19@yahoo.com>
2023-07-19 19:50:30 -0500

    Create CreateDatabase.cs

---
+++ SQLite/CreateDatabase.cs
@@ -0,0 +1,36 @@
+﻿using System.Data.SQLite;
+
+namespace IntermediateExercises.SQLite
+{
+    public class CreateDatabase
+    {
+        public static void CreationDB()
+        {
+            string dbFileName = "out.sqlite";
+
+            if (!File.Exists(dbFileName))
+            {
+                SQLiteConnection.CreateFile(dbFileName);
+            }
+
+            using (SQLiteConnection connection = new SQLiteConnection(
+                "Data Source=" + dbFileName + ";Version=3;"
+            ))
+            {
+                connection.Open();
+
+                string personTable = "create table if not exists person (name varchar(20), age int)";
+                using (SQLiteCommand command = new SQLiteCommand(personTable, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                string teacherTable = "create table if not exists teacher (name varchar(20))";
+                using (SQLiteCommand command = new SQLiteCommand(teacherTable, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
