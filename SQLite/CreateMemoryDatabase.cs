c1e4893b2d260c91ff56eb03b405679c2d4f43b5
Animeology <joseph.nguyen19@yahoo.com>
2023-07-19 19:50:31 -0500

    Create CreateMemoryDatabase.cs

---
+++ SQLite/CreateMemoryDatabase.cs
@@ -0,0 +1,33 @@
+﻿using System.Data.SQLite;
+
+namespace IntermediateExercises.SQLite
+{
+    public class CreateMemoryDatabase
+    {
+        public static void CreateMemoryDB()
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(
+                    "Data Source=" +
+                    ":memory:" +
+                    ";Version=3;" +
+                    "New=True"
+                )
+            )
+            {
+                connection.Open();
+
+                string personTableString = "create table person (name varchar(20), age int)";
+                using (SQLiteCommand command = new SQLiteCommand(personTableString, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                string teacherTableString = "create table teacher (name varchar(20))";
+                using (SQLiteCommand command = new SQLiteCommand(teacherTableString, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
