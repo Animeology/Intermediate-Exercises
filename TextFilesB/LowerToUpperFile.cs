0c9192778c1c14987dfd664465f108c8bb272953
Animeology <joseph.nguyen19@yahoo.com>
2023-07-20 12:57:35 -0500

    Create LowerToUpperFile.cs

---
+++ TextFilesB/LowerToUpperFile.cs
@@ -0,0 +1,28 @@
+﻿namespace IntermediateExercises.TextFilesB
+{
+    using Base;
+
+    public class LowerToUpperFile
+    {
+        public static void ChangeLowerToUpperFile()
+        {
+            Printing.PrintLine("Creates a new file that uppercase every word in the input.txt file");
+            string file = "input.txt";
+            string newFile = "output.txt";
+
+            using (StreamReader sr = new StreamReader(file))
+            {
+                using (StreamWriter sw = new StreamWriter(newFile))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()!) != null)
+                    {
+                        string upperLines = line.ToUpper();
+
+                        sw.WriteLine(upperLines);
+                    }
+                }
+            }
+        }
+    }
+}
