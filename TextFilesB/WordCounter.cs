0a24ab21554d4a5d03dfd7c48e54bf70280e4eaf
Animeology <joseph.nguyen19@yahoo.com>
2023-07-20 12:57:32 -0500

    Create WordCounter.cs

---
+++ TextFilesB/WordCounter.cs
@@ -0,0 +1,30 @@
+﻿namespace IntermediateExercises.TextFilesB
+{
+    using Base;
+
+    public class WordCounter
+    {
+        public static void CountWordsInFile()
+        {
+            Printing.PrintLine("Tt will count the number of words in the input.txt file");
+            string file = "input.txt";
+            int count = 0;
+
+            using (StreamReader sr = new StreamReader(file))
+            {
+                string line;
+                do
+                {
+                    line = sr.ReadLine()!;
+
+                    if (line != null)
+                    {
+                        count += line.Split(' ').Length;
+                    }
+                } while (line != null);
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
