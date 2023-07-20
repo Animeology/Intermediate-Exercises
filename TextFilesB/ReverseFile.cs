e1b6e17d9ecf097c92637c6a5eb2f31b602ff8c5
Animeology <joseph.nguyen19@yahoo.com>
2023-07-20 12:57:34 -0500

    Create ReverseFile.cs

---
+++ TextFilesB/ReverseFile.cs
@@ -0,0 +1,27 @@
+﻿namespace IntermediateExercises.TextFilesB
+{
+    using Base;
+
+    public class ReverseFile
+    {
+        public static void ReverseInFile()
+        {
+            Printing.PrintLine("Creates a new file that reverse every word in the input.txt file");
+            string file = "input.txt";
+            string newFile = "input.inv";
+
+            using (StreamReader sr = new StreamReader(file))
+            {
+                using (StreamWriter sw = new StreamWriter(newFile))
+                {
+                    string[] lines = File.ReadAllLines(file);
+
+                    for (int i = lines.Length - 1; i >= 0; i--)
+                    {
+                        sw.WriteLine(lines[i]);
+                    }
+                }
+            }
+        }
+    }
+}
