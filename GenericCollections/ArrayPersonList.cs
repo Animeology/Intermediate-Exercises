78079965bcf454bfedeb75b949fd7135e96c4052
Animeology <joseph.nguyen19@yahoo.com>
2023-07-21 17:24:06 -0500

    Create ArrayPersonList.cs

---
+++ GenericCollections/ArrayPersonList.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+
+namespace IntermediateExercises.GenericCollections
+{
+    using Base;
+    using Structures;
+
+    public class ArrayPersonList
+    {
+        ArrayList list = new ArrayList();
+
+        const int ARRAY_COUNT = 3;
+        void CreatePerson()
+        {
+            Printing.PrintLine("Create three Person with their name and age and it will print the three Person");
+
+            for (int i = 0; i < ARRAY_COUNT; i++)
+            {
+                string name = InputChecker.InputStringOnlyLetters();
+                int age = InputChecker.InputInt();
+
+                list.Add(new Person()
+                {
+                    m_name = name,
+                    m_age = age
+                });
+            }            
+        }
+
+        public void PrintPerson()
+        {
+            CreatePerson();
+
+            foreach (Person p in list)
+            {
+                Printing.PrintLine(p.ToString());
+            }
+        }
+    }
+}
