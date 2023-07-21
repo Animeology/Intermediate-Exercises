695e630642d1327aea1fc44676cff79572ed6211
Animeology <joseph.nguyen19@yahoo.com>
2023-07-21 17:24:58 -0500

    commented everything and ArrayPersonList works

--- IntermediateExercises.cs
+++ IntermediateExercises.cs
@@ -17,118 +17,123 @@
     using Recursion;
     using TextFilesA;
     using TextFilesB;
+    using GenericCollections;
 
     class IntermediateExercises
     {
         public static void Main(string[] args)
         {
-            // Arithmetic
-            FunctionCalculator.CalculateFunction();
+            //// Arithmetic
+            //FunctionCalculator.CalculateFunction();
 
-            // Arrays
-            ReverseArray.ReversingTheArray();
-            SearchArray.SearchCertainArray();
-            SearchEvenArray.SearchEvenNums();
-            SearchPostiveNegativeArray.SearchPosAndNeg();
-            BubbleSortArray.BubbleSort();
-            TwoDeeArray.TwoDeeArrays();
-            TwoDeeCharArray.DrawTwoDeeCharArray();
-            StructureArray.ArrayStruct();
-            MathStatisticsArray.MathematicalStatistics();
-            Circumference.CalculateCircumference();
+            //// Arrays
+            //ReverseArray.ReversingTheArray();
+            //SearchArray.SearchCertainArray();
+            //SearchEvenArray.SearchEvenNums();
+            //SearchPostiveNegativeArray.SearchPosAndNeg();
+            //BubbleSortArray.BubbleSort();
+            //TwoDeeArray.TwoDeeArrays();
+            //TwoDeeCharArray.DrawTwoDeeCharArray();
+            //StructureArray.ArrayStruct();
+            //MathStatisticsArray.MathematicalStatistics();
+            //Circumference.CalculateCircumference();
 
-            // Binary Files
-            ReadIDEFile.ReadID3();
-            ReadBMPFile.ReadBMP();
-            BMPImageFile.BMPImage();
-            InvertedBinaryFile.InvertedFile();
+            //// Binary Files
+            //ReadIDEFile.ReadID3();
+            //ReadBMPFile.ReadBMP();
+            //BMPImageFile.BMPImage();
+            //InvertedBinaryFile.InvertedFile();
 
-            // Data Types B
-            HexaTable.HexadecimalTable();
-            BinaryHexa.BinaryHexadecimal();
-            PrimeFactor.PrimeFactors();
+            //// Data Types B
+            //HexaTable.HexadecimalTable();
+            //BinaryHexa.BinaryHexadecimal();
+            //PrimeFactor.PrimeFactors();
 
-            // Flow Controls A
-            MathematicalStatistics.MathStatistics();
+            //// Flow Controls A
+            //MathematicalStatistics.MathStatistics();
 
-            // Flow Controls B
-            RepeatingStructures.RepetitiveStructures();
-            InfiniteDivide.KeepOnDividing();
-            AccessControl.Login();
-            Multiples.MultipleThreesAndFives();
+            //// Flow Controls B
+            //RepeatingStructures.RepetitiveStructures();
+            //InfiniteDivide.KeepOnDividing();
+            //AccessControl.Login();
+            //Multiples.MultipleThreesAndFives();
 
-            // Flow Controls C
-            WhileLoopForChange.MoneyChange();
-            Alphabet.DisplayAlphabet();
-            ConditionalOperation.ConditionalOp();
-            NestedConditional.NestedConditionalOp();
+            //// Flow Controls C
+            //WhileLoopForChange.MoneyChange();
+            //Alphabet.DisplayAlphabet();
+            //ConditionalOperation.ConditionalOp();
+            //NestedConditional.NestedConditionalOp();
 
-            // Functions A
-            CountSpaces.SpaceFunction();
-            FormatText.Formatting();
-            CalculateDouble.CalculateDoubles();
-            CalculateUsingReference.CalculateRefDouble();
-            SwapParameter.SwapParameters();
-            AddingIndexInArray.AddArray();
+            //// Functions A
+            //CountSpaces.SpaceFunction();
+            //FormatText.Formatting();
+            //CalculateDouble.CalculateDoubles();
+            //CalculateUsingReference.CalculateRefDouble();
+            //SwapParameter.SwapParameters();
+            //AddingIndexInArray.AddArray();
 
-            // Functions B
-            ModifyCharacters.ModifyCharacter();
-            AddFunction.Add();
-            ReverseString.StringReverse();
-            SearchMaxValue.GreatestValue();
-            PowerOperation.PowerOp();
-            TitleFormat.Title();
+            //// Functions B
+            //ModifyCharacters.ModifyCharacter();
+            //AddFunction.Add();
+            //ReverseString.StringReverse();
+            //SearchMaxValue.GreatestValue();
+            //PowerOperation.PowerOp();
+            //TitleFormat.Title();
 
-            // Functions C
-            AlphabetChecker.CheckAlphabet();
-            NumberChecker.CheckNumber();
-            MainFunctionCalculator.MainParameterCalculator(args);
-            MainFunctionCalculatorWithReturn.MainParameterCalculatorReturn(args);
-            MaxToMinArray.MaxMinArray();
-            PalindromeFunction.Palindrome();
+            //// Functions C
+            //AlphabetChecker.CheckAlphabet();
+            //NumberChecker.CheckNumber();
+            //MainFunctionCalculator.MainParameterCalculator(args);
+            //MainFunctionCalculatorWithReturn.MainParameterCalculatorReturn(args);
+            //MaxToMinArray.MaxMinArray();
+            //PalindromeFunction.Palindrome();
 
-            // Geometry A
-            TriangleShape.Triangle();
-            NorthEastTriangle.NETriangle();
-            RightTriangle.RightAlignTriangle();
-            Graphics.GraphicFunction();
+            //// Geometry A
+            //TriangleShape.Triangle();
+            //NorthEastTriangle.NETriangle();
+            //RightTriangle.RightAlignTriangle();
+            //Graphics.GraphicFunction();
 
-            // Geometry B
-            HollowSquareShape.HollowSquare();
-            HollowRectShape.HollowRectangle();
-            ParallelogramShape.Parallelogram();
-            PyramidShape.Pyramid();
+            //// Geometry B
+            //HollowSquareShape.HollowSquare();
+            //HollowRectShape.HollowRectangle();
+            //ParallelogramShape.Parallelogram();
+            //PyramidShape.Pyramid();
 
-            // OOP
-            Animal.AnimalEating();
-            DrivingCar.CarDriving();
-            Inheritance.InheritanceObjects();
-            PersonDisplay.PersonNameConAndDes();
-            PersonName.PersonNameInArray();
-            PhotobookTest.PhotoBookTest();
-            StudentAndTeacher.StudentTeacherTest();
+            //// OOP
+            //Animal.AnimalEating();
+            //DrivingCar.CarDriving();
+            //Inheritance.InheritanceObjects();
+            //PersonDisplay.PersonNameConAndDes();
+            //PersonName.PersonNameInArray();
+            //PhotobookTest.PhotoBookTest();
+            //StudentAndTeacher.StudentTeacherTest();
 
-            // Recursion
-            Factorial.FactorialRecursion();
-            Fibonacci.FibonacciRecursion();
-            Multiply.MultiplyRecursion();
-            Palindrome.PalindromeRecursion();
-            Power.PowerRecursion();
-            Reverse.ReverseRecursion();
+            //// Recursion
+            //Factorial.FactorialRecursion();
+            //Fibonacci.FibonacciRecursion();
+            //Multiply.MultiplyRecursion();
+            //Palindrome.PalindromeRecursion();
+            //Power.PowerRecursion();
+            //Reverse.ReverseRecursion();
 
-            // SQLite
-            ReadingTheDatabase.ReadingListFromDB();
-            CreateDatabase.CreationDB();
-            CreateMemoryDatabase.CreateMemoryDB();
-            CRUD.CRUDMenu();
+            //// SQLite
+            //ReadingTheDatabase.ReadingListFromDB();
+            //CreateDatabase.CreationDB();
+            //CreateMemoryDatabase.CreateMemoryDB();
+            //CRUD.CRUDMenu();
 
-            // Text Files A
-            SearchLines.SearchLinesInFile();
+            //// Text Files A
+            //SearchLines.SearchLinesInFile();
 
-            // Text Files B
-            ReverseFile.ReverseInFile();
-            LowerToUpperFile.ChangeLowerToUpperFile();
-            WordCounter.CountWordsInFile();
+            //// Text Files B
+            //ReverseFile.ReverseInFile();
+            //LowerToUpperFile.ChangeLowerToUpperFile();
+            //WordCounter.CountWordsInFile();
+
+            // Generic Collections
+            ArrayPersonList list = new ArrayPersonList();
+            list.PrintPerson();
         }
     }
 }
