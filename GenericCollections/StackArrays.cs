fa54e4571e0c94afaad5113ad96e63ff544b27be
Animeology <joseph.nguyen19@yahoo.com>
2023-07-23 16:44:35 -0500

    Create StackArrays.cs

---
+++ GenericCollections/StackArrays.cs
@@ -0,0 +1,30 @@
+﻿namespace IntermediateExercises.GenericCollections
+{
+    public class StackArrays
+    {
+        int[] intArr;
+        int currentPos;
+
+        public StackArrays(int size)
+        {
+            intArr = new int[size];
+            currentPos = -1;
+        }
+
+        public void Push(int value)
+        {
+            currentPos++;
+            intArr[currentPos] = value;
+        }
+
+        public int Pop()
+        {
+            if(currentPos > 0)
+            {
+                currentPos -= 1;
+                return intArr[currentPos];
+            }
+            return 0;
+        }
+    }
+}
