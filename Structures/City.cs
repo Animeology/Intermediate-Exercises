23d9c4d66943a8103afb70c0fdfb1027da94f280
Animeology <joseph.nguyen19@yahoo.com>
2023-07-24 15:53:52 -0500

    Create City.cs

---
+++ Structures/City.cs
@@ -0,0 +1,9 @@
+﻿namespace IntermediateExercises.Structures
+{
+    [Serializable]
+    public class City
+    {
+        public string? Name { get; set; }
+        public int Population { get; set; }
+    }
+}
